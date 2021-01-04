badcb1d3b5ec26136f7529a4062157763b2cc5ec
Craig Gidney <craig.gidney@gmail.com>
2021-01-04 14:39:07 -0400

    surface code profile test

--- main.cc
+++ main.cc
@@ -5,37 +5,124 @@
 #include <sstream>
 #include "simd_util.h"
 #include "pauli_string.h"
+#include "chp_sim.h"
 #include <chrono>
 #include <random>
+#include <complex>
 
-int main() {
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<unsigned long long> dis(
-            std::numeric_limits<std::uint64_t>::min(),
-            std::numeric_limits<std::uint64_t>::max());
-    constexpr size_t w = 256 * 256;
-    size_t num_bits = w * w;
-    size_t num_u64 = num_bits / 64;
-    auto data = (uint64_t *) _mm_malloc(num_u64 * sizeof(uint64_t), 32);
-    for (size_t k = 0; k < num_u64; k++) {
-        data[k] = dis(gen);
+void run_surface_code_sim(size_t distance) {
+    size_t diam = distance * 2 - 1;
+    auto qubit = [&](std::complex<float> c){
+        return (size_t)(c.real() * diam + c.imag());
+    };
+    auto in_range = [=](std::complex<float> c) {
+        return c.real() >= 0 && c.real() < (float)diam
+            && c.imag() >= 0 && c.imag() < (float)diam;
+    };
+    std::vector<std::complex<float>> data;
+    std::vector<std::complex<float>> xs;
+    std::vector<std::complex<float>> zs;
+    auto sim = ChpSim(diam*diam);
+    for (size_t x = 0; x < diam; x++) {
+        for (size_t y = 0; y < diam; y++) {
+            if (x % 2 == 1 && y % 2 == 0) {
+                xs.emplace_back((float)x, (float)y);
+            } else if (x % 2 == 0 && y % 2 == 1) {
+                zs.emplace_back((float)x, (float)y);
+            } else {
+                data.emplace_back((float)x, (float)y);
+            }
+        }
     }
+    std::vector<std::complex<float>> dirs {
+            {1, 0},
+            {0, 1},
+            {0, -1},
+            {-1, 0},
+    };
+    for (size_t round = 0; round < distance; round++) {
+        std::cerr << "round " << round << "\n";
+        for (const auto &x : xs) {
+            sim.hadamard(qubit(x));
+        }
+        std::cerr << "cnots " << round << "\n";
+        for (const auto &d : dirs) {
+            for (const auto &z : zs) {
+                auto p = z + d;
+                if (in_range(p)) {
+                    sim.cnot(qubit(p), qubit(z));
+                }
+            }
+            for (const auto &x : xs) {
+                auto p = x + d;
+                if (in_range(p)) {
+                    sim.cnot(qubit(x), qubit(p));
+                }
+            }
+        }
+        std::cerr << "z measure " << round << "\n";
+        for (const auto &z : zs) {
+            assert(sim.is_deterministic(qubit(z)));
+            sim.measure(qubit(z));
+        }
+        std::cerr << "x measure " << round << "\n";
+        for (const auto &x : xs) {
+            sim.hadamard(qubit(x));
+        }
+        for (const auto &x : xs) {
+            if (round == 0) {
+                std::cerr << "x measure " << x << "," << round << "\n";
+            }
+            assert(sim.is_deterministic(qubit(x)) == round > 0);
+            sim.measure(qubit(x));
+        }
+    }
+    for (auto d : data) {
+        std::cerr << "data measure " << d << "\n";
+        sim.measure(qubit(d));
+    }
+}
 
+void time_clifford_sim(size_t reps, size_t distance) {
     auto start = std::chrono::steady_clock::now();
-    size_t n = 10;
-    for (size_t i = 0; i < n; i++) {
-        transpose_bit_matrix(data, w);
+    for (size_t rep = 0; rep < reps; rep++) {
+        run_surface_code_sim(distance);
     }
     auto end = std::chrono::steady_clock::now();
     auto dt = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() / 1000.0 / 1000.0;
-    std::cout << n / dt << " transposes/sec (" << w << "x" << w << ", " << (num_bits >> 23) << " MiB)\n";
+    std::cout << dt / reps << " sec/eval " << distance << "\n";
+}
 
-    start = std::chrono::steady_clock::now();
-    for (size_t i = 0; i < n; i++) {
-        transpose_bit_matrix_256x256blocks(data, w);
-    }
-    end = std::chrono::steady_clock::now();
-    dt = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() / 1000.0 / 1000.0;
-    std::cout << n / dt << " block transposes/sec (" << w << "x" << w << ", " << (num_bits >> 23) << " MiB)\n";
+int main() {
+//    std::random_device rd;
+//    std::mt19937 gen(rd());
+//    std::uniform_int_distribution<unsigned long long> dis(
+//            std::numeric_limits<std::uint64_t>::min(),
+//            std::numeric_limits<std::uint64_t>::max());
+//    constexpr size_t w = 256 * 64;
+//    size_t num_bits = w * w;
+//    size_t num_u64 = num_bits / 64;
+//    auto data = (uint64_t *) _mm_malloc(num_u64 * sizeof(uint64_t), 32);
+//    for (size_t k = 0; k < num_u64; k++) {
+//        data[k] = dis(gen);
+//    }
+//
+//    auto start = std::chrono::steady_clock::now();
+//    size_t n = 10;
+//    for (size_t i = 0; i < n; i++) {
+//        transpose_bit_matrix(data, w);
+//    }
+//    auto end = std::chrono::steady_clock::now();
+//    auto dt = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() / 1000.0 / 1000.0;
+//    std::cout << n / dt << " transposes/sec (" << w << "x" << w << ", " << (num_bits >> 23) << " MiB)\n";
+//
+//    start = std::chrono::steady_clock::now();
+//    for (size_t i = 0; i < n; i++) {
+//        transpose_bit_matrix_256x256blocks(data, w);
+//    }
+//    end = std::chrono::steady_clock::now();
+//    dt = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() / 1000.0 / 1000.0;
+//    std::cout << n / dt << " block transposes/sec (" << w << "x" << w << ", " << (num_bits >> 23) << " MiB)\n";
+
+    time_clifford_sim(1, 25);
 }
