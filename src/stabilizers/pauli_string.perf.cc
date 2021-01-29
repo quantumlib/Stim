bc154458fc0cb61bae55e6a56a69111e0bf34b7a
Craig Gidney <craiggidney@google.com>
2021-01-28 22:09:19 -0400

    Recalibrate Pauli multiplication benchmark - It seems as though the reference might have been collected when the optimizer was removing the popcount code due to undefined behavior

--- src/stabilizers/pauli_string.perf.cc
+++ src/stabilizers/pauli_string.perf.cc
@@ -2,12 +2,21 @@
 
 #include "../benchmark_util.h"
 
+BENCHMARK(PauliString_multiplication_1M) {
+    size_t n = 1000 * 1000;
+    PauliString p1(n);
+    PauliString p2(n);
+    benchmark_go([&]() { p1.ref().inplace_right_mul_returning_log_i_scalar(p2); })
+        .goal_micros(8.5)
+        .show_rate("Paulis", n);
+}
+
 BENCHMARK(PauliString_multiplication_100K) {
     size_t n = 100 * 1000;
     PauliString p1(n);
     PauliString p2(n);
     benchmark_go([&]() { p1.ref().inplace_right_mul_returning_log_i_scalar(p2); })
-        .goal_nanos(500)
+        .goal_nanos(700)
         .show_rate("Paulis", n);
 }
 
@@ -16,6 +25,6 @@ BENCHMARK(PauliString_multiplication_10K) {
     PauliString p1(n);
     PauliString p2(n);
     benchmark_go([&]() { p1.ref().inplace_right_mul_returning_log_i_scalar(p2); })
-        .goal_nanos(50)
+        .goal_nanos(90)
         .show_rate("Paulis", n);
 }
