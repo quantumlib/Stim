901bcd2b6fb0e3c555bdfc09313284c61ba4f0c4
Craig Gidney <craiggidney@google.com>
2021-04-15 16:27:06 -0300

    Workaroud std::random_device bug (#29)

--- src/probability_util.cc
+++ src/probability_util.cc
@@ -36,7 +36,13 @@ std::vector<size_t> sample_hit_indices(float probability, size_t attempts, std::
 }
 
 std::mt19937_64 externally_seeded_rng() {
+#if defined(__linux) && defined(__GLIBCXX__) && __GLIBCXX__ >= 20200128
+    // Workaround for https://gcc.gnu.org/bugzilla/show_bug.cgi?id=94087
+    // See https://github.com/quantumlib/Stim/issues/26
+    std::random_device d("/dev/urandom");
+#else
     std::random_device d;
+#endif
     std::seed_seq seq{d(), d(), d(), d(), d(), d(), d(), d()};
     std::mt19937_64 result(seq);
     return result;
