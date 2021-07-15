e96602e81448275477c483506a127f2928ad9a52
Craig Gidney <craiggidney@google.com>
2021-07-15 16:24:43 -0700

    Build lenience

--- src/circuit/gate_data_swaps.cc
+++ src/circuit/gate_data_swaps.cc
@@ -7,7 +7,7 @@
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is     distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -22,7 +22,6 @@
 using namespace stim_internal;
 
 static constexpr std::complex<float> i = std::complex<float>(0, 1);
-static constexpr std::complex<float> s = 0.7071067811865475244f;
 
 void GateDataMap::add_gate_data_swaps(bool &failed) {
     add_gate(
