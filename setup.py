f81a63a81a28cf34483f413c67554747f32bb124
Craig Gidney <craiggidney@google.com>
2021-12-13 20:35:02 -0800

    v1.7.0

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ MAIN_FILES = glob.glob("src/**/main.cc", recursive=True)
 HEADER_FILES = glob.glob("src/**/*.h", recursive=True)
 RELEVANT_SOURCE_FILES = sorted(set(ALL_SOURCE_FILES) - set(TEST_FILES + PERF_FILES + MAIN_FILES))
 
-version = '1.7.dev1'
+version = '1.7.0'
 
 common_compile_args = [
     '-std=c++11',
