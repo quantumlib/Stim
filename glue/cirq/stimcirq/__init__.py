3906b18e3d1f7b1686819c3344ed6889055174fc
Craig Gidney <craiggidney@google.com>
2022-01-07 18:00:09 -0800

    sort imports

--- glue/cirq/stimcirq/__init__.py
+++ glue/cirq/stimcirq/__init__.py
@@ -1,8 +1,8 @@
 from ._cirq_to_stim import cirq_circuit_to_stim_circuit
-from ._stim_sampler import StimSampler
-from ._stim_to_cirq import stim_circuit_to_cirq_circuit, MeasureAndOrResetGate, TwoQubitAsymmetricDepolarizingChannel
 from ._det_annotation import DetAnnotation
 from ._obs_annotation import CumulativeObservableAnnotation
+from ._stim_sampler import StimSampler
+from ._stim_to_cirq import stim_circuit_to_cirq_circuit, MeasureAndOrResetGate, TwoQubitAsymmetricDepolarizingChannel
 
 JSON_RESOLVER = {
     "CumulativeObservableAnnotation": CumulativeObservableAnnotation._from_json_helper,
@@ -13,11 +13,11 @@ JSON_RESOLVER = {
 
 # Workaround for doctest not searching imported objects.
 __test__ = {
-    "StimSampler": StimSampler,
     "cirq_circuit_to_stim_circuit": cirq_circuit_to_stim_circuit,
-    "stim_circuit_to_cirq_circuit": stim_circuit_to_cirq_circuit,
-    "MeasureAndOrResetGate": MeasureAndOrResetGate,
-    "TwoQubitAsymmetricDepolarizingChannel": TwoQubitAsymmetricDepolarizingChannel,
     "CumulativeObservableAnnotation": CumulativeObservableAnnotation,
     "DetAnnotation": DetAnnotation,
+    "MeasureAndOrResetGate": MeasureAndOrResetGate,
+    "stim_circuit_to_cirq_circuit": stim_circuit_to_cirq_circuit,
+    "StimSampler": StimSampler,
+    "TwoQubitAsymmetricDepolarizingChannel": TwoQubitAsymmetricDepolarizingChannel,
 }
