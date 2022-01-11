5592b549fbd99712c9927ab1b79ed15dbf9f4c98
Craig Gidney <craiggidney@google.com>
2022-01-10 16:53:13 -0800

    iterate

--- glue/python/overwrite_dev_version_with_date.sh
+++ glue/python/overwrite_dev_version_with_date.sh
@@ -17,8 +17,10 @@ cd "$(git rev-parse --show-toplevel)"
 
 # Generate dev version starting from major.minor version.
 # (Requires the existing version to have a 'dev' suffix.)
+# (Uses the timestamp of the HEAD commit, to ensure consistency when run multiple times.)
 readonly MAJOR_MINOR_VERSION="$(cat setup.py | sed -n "s/version.*=.*'\(.*\)\.dev.*'/\1/p")"
-readonly DEV_VERSION="${MAJOR_MINOR_VERSION}.dev$(date "+%Y%m%d%H%M%S")"
+readonly TIMESTAMP="$(date -d @"$(git show -s --format=%ct HEAD)" "+%Y%m%d%H%M%S")"
+readonly DEV_VERSION="${MAJOR_MINOR_VERSION}.dev${TIMESTAMP}"
 
 # Overwrite existing versions.
 ls .
