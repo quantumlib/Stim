5e3f4b96f19a77a66106c5d5715b19a8b93c2edf
Craig Gidney <craiggidney@google.com>
2022-01-21 18:33:21 -0800

    Seed bump

--- src/stim/circuit/circuit.h
+++ src/stim/circuit/circuit.h
@@ -34,7 +34,7 @@
 namespace stim {
 
 // Change this number from time to time to ensure people don't rely on seeds across versions.
-constexpr uint64_t INTENTIONAL_VERSION_SEED_INCOMPATIBILITY = 0xDEADBEEF1234ULL;
+constexpr uint64_t INTENTIONAL_VERSION_SEED_INCOMPATIBILITY = 0xDEADBEEF1235ULL;
 
 uint64_t op_data_rep_count(const OperationData &data);
 
