8c86340aa013ba988cca8b7bfe66a37798b23b45
Craig Gidney <craiggidney@google.com>
2022-05-24 14:54:53 -0700

    Stop adding dummy edges to ensure one connected component for pymatching (#244)
    - pymatching no longer requires the input to have just one connected component
    - Use new 'fault_ids' instead of old 'qubit_id' on edges
    - Add a test that invincible observables work correctly
    - Loosen sinter's declared requirements

--- glue/sample/src/sinter/decoding_pymatching.py
+++ glue/sample/src/sinter/decoding_pymatching.py
@@ -23,11 +23,11 @@ def decode_using_pymatching(*,
     assert bit_packed_det_samples.shape[1] == (num_dets + 7) // 8
 
     predictions = np.zeros(shape=(num_shots, num_obs), dtype=np.bool8)
+    # note: extra 2 are the boundary node and the invincible-observable-boundary-edge node
+    buffer = np.zeros(num_dets + 2, dtype=np.bool8)
     for k in range(num_shots):
-        expanded_det = np.unpackbits(bit_packed_det_samples[k], count=num_dets, bitorder='little')
-        expanded_det = np.resize(expanded_det, num_dets + 1)
-        expanded_det[-1] = 0
-        predictions[k] = matching_graph.decode(expanded_det)
+        buffer[:num_dets] = np.unpackbits(bit_packed_det_samples[k], count=num_dets, bitorder='little')
+        predictions[k] = matching_graph.decode(buffer)
     return predictions
 
 
@@ -103,7 +103,7 @@ def detector_error_model_to_nx_graph(model: stim.DetectorErrorModel) -> nx.Graph
         if g.has_edge(*dets):
             edge_data = g.get_edge_data(*dets)
             old_p = edge_data["error_probability"]
-            old_frame_changes = edge_data["qubit_id"]
+            old_frame_changes = edge_data["fault_ids"]
             # If frame changes differ, the code has distance 2; just keep whichever was first.
             if set(old_frame_changes) == set(frame_changes):
                 p = p * (1 - old_p) + old_p * (1 - p)
@@ -111,7 +111,7 @@ def detector_error_model_to_nx_graph(model: stim.DetectorErrorModel) -> nx.Graph
         if p > 0.5:
             p = 1 - p
         if p > 0:
-            g.add_edge(*dets, weight=math.log((1 - p) / p), qubit_id=frame_changes, error_probability=p)
+            g.add_edge(*dets, weight=math.log((1 - p) / p), fault_ids=frame_changes, error_probability=p)
 
     def handle_detector_coords(detector: int, coords: np.ndarray):
         g.add_node(detector, coords=coords)
@@ -127,15 +127,11 @@ def detector_error_model_to_pymatching_graph(model: stim.DetectorErrorModel) -> 
     num_detectors = model.num_detectors
     num_observables = model.num_observables
 
-    # Add spandrels to the graph to ensure pymatching will accept it.
-    # - Make sure there's only one connected component.
-    # - Make sure no detector nodes are skipped.
-    # - Make sure no observable nodes are skipped.
+    # Ensure invincible detectors are seen by explicitly adding a node for each detector.
     for k in range(num_detectors):
         g.add_node(k)
+    # Ensure invincible observables are seen by adding a boundary edge with all observables.
     g.add_node(num_detectors + 1)
-    for k in range(num_detectors + 1):
-        g.add_edge(k, num_detectors + 1, weight=9999999999)
-    g.add_edge(num_detectors, num_detectors + 1, weight=9999999999, qubit_id=list(range(num_observables)))
+    g.add_edge(num_detectors, num_detectors + 1, weight=1, fault_ids=list(range(num_observables)))
 
     return pymatching.Matching(g)
