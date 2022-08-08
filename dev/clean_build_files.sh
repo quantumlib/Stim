0906bf28fa99051b02d37a3e63e7d8b44d43de31
Craig Gidney <craiggidney@google.com>
2022-08-07 21:24:50 -0700

    Test some uncovered code in stim::Circuit (#324)
    - Fix `stim.Circuit.flattened` and `stim.Circuit.without_noise` not fusing all operations
    - Fix `PAULI_CHANNEL_2` not being marked as targeting pairs (!! enables read outside bounds !!)
    - Add dev/ directory for scripts
    - Add dev/clean_build_files.sh
    - Add dev/doctest_proper.py
    - Test stim::Circuit::approx_equals and stim::Circuit::operator{==,!=}

---
+++ dev/clean_build_files.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+#########################################################################
+# Deletes files created by cmake, python setup.py, and other build steps.
+#########################################################################
+
+# Get to this script's git repo root.
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+cd "$(git rev-parse --show-toplevel)"
+
+rm CMakeFiles -rf
+rm CMakeCache.txt -f
+rm Makefile -f
+rm dist -rf
+rm cmake_install.cmake -f
+rm cmake-build-debug-coverage -rf
+rm coverage -rf
+rm stim.egg-info -rf
+rm bazel-bin -rf
+rm bazel-out -rf
+rm bazel-stim -rf
+rm bazel-testlogs -rf
+rm Testing -rf
+rm out -rf
