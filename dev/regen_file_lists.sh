a1c6ea1823d5888063cd5a39e5804df494f10e3c
Daniel Barter <danielbarter@google.com>
2022-08-25 11:23:09 -0700

    comment explaining LC_ALL=C

--- dev/regen_file_lists.sh
+++ dev/regen_file_lists.sh
@@ -17,6 +17,7 @@ echo "generating file lists in $FOLDER"
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 cd "$(git rev-parse --show-toplevel)"
 
+# LC_ALL=C forces sorting to happen by byte value
 find src | grep \.cc | grep -Ev "test|pybind|perf|^src/main\.cc" | LC_ALL=C sort > $FOLDER/source_files_no_main
 find src | grep \.cc | grep test | LC_ALL=C sort > $FOLDER/test_files
 find src | grep \.cc | grep perf | LC_ALL=C sort > $FOLDER/benchmark_files
