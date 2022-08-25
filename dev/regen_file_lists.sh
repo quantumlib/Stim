58cbc48ab6e1a7e83540707865f37751aad8679a
Daniel Barter <danielbarter@google.com>
2022-08-25 10:56:37 -0700

    sort file lists alphabetically

--- dev/regen_file_lists.sh
+++ dev/regen_file_lists.sh
@@ -17,7 +17,7 @@ echo "generating file lists in $FOLDER"
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 cd "$(git rev-parse --show-toplevel)"
 
-find src | grep \.cc | grep -Ev "test|pybind|perf|^src/main\.cc" > $FOLDER/source_files_no_main
-find src | grep \.cc | grep test > $FOLDER/test_files
-find src | grep \.cc | grep perf > $FOLDER/benchmark_files
-find src | grep \.cc | grep pybind > $FOLDER/python_api_files
+find src | grep \.cc | grep -Ev "test|pybind|perf|^src/main\.cc" | sort > $FOLDER/source_files_no_main
+find src | grep \.cc | grep test | sort > $FOLDER/test_files
+find src | grep \.cc | grep perf | sort > $FOLDER/benchmark_files
+find src | grep \.cc | grep pybind | sort > $FOLDER/python_api_files
