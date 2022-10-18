b773b11f8ae33a350fde19823b061f762aa877d8
Craig Gidney <craiggidney@google.com>
2022-10-18 00:58:23 -0700

    Add detector slice diagrams - Auto-generate the stim.h header file when regenerating file lists

--- dev/regen_file_lists.sh
+++ dev/regen_file_lists.sh
@@ -22,3 +22,12 @@ find src | grep "\\.cc$" | grep -v "\\.\(test\|perf\|pybind\)\\.cc$" | grep -v "
 find src | grep "\\.test\\.cc$" | LC_ALL=C sort > "${FOLDER}/test_files"
 find src | grep "\\.perf\\.cc$" | LC_ALL=C sort > "${FOLDER}/benchmark_files"
 find src | grep "\\.pybind\\.cc$" | LC_ALL=C sort > "${FOLDER}/python_api_files"
+
+# Regenerate 'stim.h' to include all relevant headers.
+echo "#ifndef _STIM_H" > src/stim.h
+echo "#define _STIM_H" >> src/stim.h
+echo "/// WARNING: THE STIM C++ API MAKES NO COMPATIBILITY GUARANTEES." >> src/stim.h
+echo "/// It may change arbitrarily and catastrophically from minor version to minor version." >> src/stim.h
+echo "/// If you need a stable API, use stim's Python API." >> src/stim.h
+find src | grep "\\.h$" | grep -v "\\.\(test\|perf\|pybind\)\\.h$" | grep -v "src/stim\\.h" | LC_ALL=C sort | sed 's/src\/\(.*\)/#include "\1"/g' >> "src/stim.h"
+echo "#endif" >> src/stim.h
