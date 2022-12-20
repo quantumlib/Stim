8e350a1581a140f906e46659101781e2b0326528
Matt McEwen <mmcewen@google.com>
2022-12-19 16:22:31 -0800

    constexpr?

--- src/stim/diagram/diagram_util.h
+++ src/stim/diagram/diagram_util.h
@@ -23,12 +23,12 @@
 
 namespace stim_draw_internal {
 
-const char * Z_BLUE = "#4DA6FF";
-const char * X_RED = "#FF4040";
-const char * Y_GREEN = "#59FF7A";
-const char * EZ_ORANGE = "#FF9500";
-const char * EX_PURPLE = "#FF4DDB";
-const char * EY_YELLOW = "#F1FF59";
+constexpr char * Z_BLUE = "#4DA6FF";
+constexpr char * X_RED = "#FF4040";
+constexpr char * Y_GREEN = "#59FF7A";
+constexpr char * EZ_ORANGE = "#FF9500";
+constexpr char * EX_PURPLE = "#FF4DDB";
+constexpr char * EY_YELLOW = "#F1FF59";
 
 size_t utf8_char_count(const std::string &s);
 
