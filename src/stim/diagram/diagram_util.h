0953e372d0f5e58e8120b18d4c7680cdde55e918
Matthew McEwen <35579799+mmcewen-g@users.noreply.github.com>
2022-12-21 19:47:54 -0500

    Adjust colors used in detector slice diagrams (#447)
    Adjust colors used in detector slice diagrams

--- src/stim/diagram/diagram_util.h
+++ src/stim/diagram/diagram_util.h
@@ -23,6 +23,14 @@
 
 namespace stim_draw_internal {
 
+
+constexpr const char * X_RED = "#FF4040";
+constexpr const char * Y_GREEN = "#59FF7A";
+constexpr const char * Z_BLUE = "#4DA6FF";
+constexpr const char * EX_PURPLE = "#FF4DDB";
+constexpr const char * EY_YELLOW = "#F1FF59";
+constexpr const char * EZ_ORANGE = "#FF9500";
+
 size_t utf8_char_count(const std::string &s);
 
 /// Splits a two qubit gate into two end pieces, which can be drawn independently.
