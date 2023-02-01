277b34ed615cf0f32d08317f1221d176631b9fcd
Craig Gidney <craiggidney@google.com>
2023-01-31 21:07:02 -0800

    Add crumble-up-to-date verification to ci (#473)
    Fixes https://github.com/quantumlib/Stim/issues/466

--- dev/regen_crumble_to_cpp_string.sh
+++ dev/regen_crumble_to_cpp_string.sh
@@ -5,22 +5,4 @@ set -e
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 cd "$(git rev-parse --show-toplevel)"
 
-{
-    echo '#include "stim/diagram/crumble_data.h"';
-    echo '';
-    echo 'std::string stim_draw_internal::make_crumble_html() {';
-    echo '    std::string result;'
-    {
-        cat glue/crumble/crumble.html | grep -v "^<script";
-        echo "<script>";
-        rollup glue/crumble/main.js | uglifyjs -c -m --mangle-props --toplevel;
-        echo "</script>";
-    } | python -c '
-import sys
-for line in sys.stdin:
-    for k in range(0, len(line), 1024):
-        part = line[k:k+1024]
-        print(f"""    result.append(R"CRUMBLE_PART({part})CRUMBLE_PART");""")';
-    echo '    return result;'
-    echo '}';
-} > src/stim/diagram/crumble_data.cc
+dev/regen_crumble_to_cpp_string_write_to_stdout.sh > src/stim/diagram/crumble_data.cc
