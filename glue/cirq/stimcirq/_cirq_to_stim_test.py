004a6bd92dd2a4533e0a57309d6268ff12c206ac
Alexandre Bourassa <abourassa@google.com>
2023-02-23 10:41:09 -0800

    Added assertion to the loop test

--- glue/cirq/stimcirq/_cirq_to_stim_test.py
+++ glue/cirq/stimcirq/_cirq_to_stim_test.py
@@ -336,10 +336,13 @@ def test_on_loop():
                 cirq.measure(b, key="b"),
             ),
             repetitions=3,
-        )
+        ).with_tags('my_tag')
     )
     result = stimcirq.StimSampler().run(c)
     assert result.measurements.keys() == {'0:a', '0:b', '1:a', '1:b', '2:a', '2:b'}
+    
+    stim_circuit = stimcirq.cirq_circuit_to_stim_circuit(c)
+    assert stim.CircuitRepeatBlock in {type(instr) for instr in stim_circuit}
 
 
 def test_random_gate_channel():
