c483b9d795501e3ad36a00c16ee08f61c9e090d0
Craig Gidney <craiggidney@google.com>
2023-06-06 21:17:14 -0700

    err

--- glue/sample/src/sinter/_anon_task_stats_test.py
+++ glue/sample/src/sinter/_anon_task_stats_test.py
@@ -18,5 +18,5 @@ def test_repr():
 
 def test_add():
     a = sinter.AnonTaskStats(shots=220, errors=30, discards=40, seconds=50)
-    b = sinter.AnonTaskStats(shots=5, errors=4, discards=3, seconds=2)
-    assert a + b == sinter.AnonTaskStats(shots=225, errors=34, discards=43, seconds=52)
+    b = sinter.AnonTaskStats(shots=50, errors=4, discards=3, seconds=2)
+    assert a + b == sinter.AnonTaskStats(shots=270, errors=34, discards=43, seconds=52)
