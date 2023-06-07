9eaf87c59150c20163916ab605ef2e69cc02bf7d
Craig Gidney <craiggidney@google.com>
2023-06-06 21:10:20 -0700

    __version__

--- dev/overwrite_dev_versions_with_date.py
+++ dev/overwrite_dev_versions_with_date.py
@@ -24,7 +24,7 @@ def main():
     # (Requires the existing version to have a 'dev' suffix.)
     # (Uses the timestamp of the HEAD commit, to ensure consistency when run multiple times.)
     with open('setup.py') as f:
-        maj_min_version_line, = [line for line in f.read().splitlines() if re.match("^version = '[^']+'", line)]
+        maj_min_version_line, = [line for line in f.read().splitlines() if re.match("^__version__ = '[^']+'", line)]
         maj_version, min_version, patch = maj_min_version_line.split()[-1].strip("'").split('.')
         if 'dev' not in patch:
             return  # Do nothing for non-dev versions.
