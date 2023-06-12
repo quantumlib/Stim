b70a9c6ccd895a72105b520ab9abde305060aaef
Craig Gidney <craiggidney@google.com>
2023-06-11 21:05:31 -0700

    merge

--- glue/sample/src/sinter/_anon_task_stats_test.py
+++ glue/sample/src/sinter/_anon_task_stats_test.py
@@ -22,7 +22,12 @@ def test_add():
     a = sinter.AnonTaskStats(shots=220, errors=30, discards=40, seconds=50)
     b = sinter.AnonTaskStats(shots=50, errors=4, discards=3, seconds=2)
     assert a + b == sinter.AnonTaskStats(shots=270, errors=34, discards=43, seconds=52)
+    assert a + sinter.AnonTaskStats() == a
 
     a = sinter.AnonTaskStats(shots=220, errors=30, discards=40, seconds=50, classified_errors=collections.Counter({'a': 10, 'b': 20}))
     b = sinter.AnonTaskStats(shots=50, errors=4, discards=3, seconds=2, classified_errors=collections.Counter({'a': 1, 'c': 3}))
     assert a + b == sinter.AnonTaskStats(shots=270, errors=34, discards=43, seconds=52, classified_errors=collections.Counter({'a': 11, 'b': 20, 'c': 3}))
+
+    assert a + sinter.AnonTaskStats() == a
+    assert sinter.AnonTaskStats() + b == b
+
