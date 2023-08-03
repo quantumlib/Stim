af719ec655a12d5208785ac11b0dc76b6c1fc3ee
Justin Ledford <justinledford@google.com>
2023-08-03 00:56:16 +0000

    make convert subcommand tests more clear

--- src/stim/cmd/command_convert.test.cc
+++ src/stim/cmd/command_convert.test.cc
@@ -19,73 +19,73 @@
 
 using namespace stim;
 
-class ConvertTest
-    : public testing::TestWithParam<
-          std::tuple<std::string, std::tuple<std::string, std::string>, std::tuple<std::string, std::string>>> {
-   protected:
-    void SetUp() override {
-        tmp.write_contents(R"CIRCUIT(
+TEST(command_convert, convert_measurements_with_circuit) {
+    RaiiTempNamedFile tmp(R"CIRCUIT(
         X 0
         M 0 1
         DETECTOR rec[-2]
         DETECTOR rec[-1]
         OBSERVABLE_INCLUDE(2) rec[-1]
       )CIRCUIT");
+
+    std::vector<std::tuple<std::string, std::string>> measurement_data{
+        std::make_tuple("01", "00\n01\n10\n11\n"),
+        std::make_tuple("b8", std::string({0x00, 0x02, 0x01, 0x03})),
+        std::make_tuple("dets", "shot\nshot M1\nshot M0\nshot M0 M1\n"),
+        std::make_tuple("hits", "\n1\n0\n0,1\n"),
+        std::make_tuple("r8", std::string({0x02, 0x01, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00}))};
+
+    for (const auto& [in_format, in_data] : measurement_data) {
+        for (const auto& [out_format, out_data] : measurement_data) {
+            ASSERT_EQ(
+                run_captured_stim_main(
+                    {"convert",
+                     "--in_format",
+                     in_format.c_str(),
+                     "--out_format",
+                     out_format.c_str(),
+                     "--circuit",
+                     tmp.path.data(),
+                     "--types=M"},
+                    in_data),
+                out_data);
+        }
     }
-
-    RaiiTempNamedFile tmp;
-};
-
-std::vector<std::tuple<std::string, std::string>> measurement_parameters{
-    std::make_tuple("01", "00\n01\n10\n11\n"),
-    std::make_tuple("b8", std::string({0x00, 0x02, 0x01, 0x03})),
-    std::make_tuple("dets", "shot\nshot M1\nshot M0\nshot M0 M1\n"),
-    std::make_tuple("hits", "\n1\n0\n0,1\n"),
-    std::make_tuple("r8", std::string({0x02, 0x01, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00}))};
-
-std::vector<std::tuple<std::string, std::string>> detection_parameters{
-    std::make_tuple("01", "10000\n11001\n00000\n01001\n"),
-    std::make_tuple("b8", std::string({0x01, 0x13, 0x00, 0x12})),
-    std::make_tuple("dets", "shot D0\nshot D0 D1 L2\nshot\nshot D1 L2\n"),
-    std::make_tuple("hits", "0\n0,1,4\n\n1,4\n"),
-    std::make_tuple("r8", std::string({0x00, 0x04, 0x00, 0x00, 0x02, 0x00, 0x05, 0x01, 0x02, 0x00}))};
-
-TEST_P(ConvertTest, convert) {
-    std::string types = std::get<0>(GetParam());
-    auto [in_format, in_data] = std::get<1>(GetParam());
-    auto [out_format, out_data] = std::get<2>(GetParam());
-    ASSERT_EQ(
-        run_captured_stim_main(
-            {"convert",
-             ("--in_format=" + in_format).c_str(),
-             ("--out_format=" + out_format).c_str(),
-             "--circuit",
-             tmp.path.data(),
-             ("--types=" + types).c_str()},
-            in_data),
-        out_data);
 }
 
-template <typename T>
-std::string GenerateTestParameterName(const testing::TestParamInfo<T>& info) {
-    std::string from = std::get<0>(std::get<1>(info.param));
-    std::string to = std::get<0>(std::get<2>(info.param));
-    return from + "_to_" + to;
+TEST(command_convert, convert_detections_with_circuit) {
+    RaiiTempNamedFile tmp(R"CIRCUIT(
+        X 0
+        M 0 1
+        DETECTOR rec[-2]
+        DETECTOR rec[-1]
+        OBSERVABLE_INCLUDE(2) rec[-1]
+      )CIRCUIT");
+
+    std::vector<std::tuple<std::string, std::string>> detection_data{
+        std::make_tuple("01", "10000\n11001\n00000\n01001\n"),
+        std::make_tuple("b8", std::string({0x01, 0x13, 0x00, 0x12})),
+        std::make_tuple("dets", "shot D0\nshot D0 D1 L2\nshot\nshot D1 L2\n"),
+        std::make_tuple("hits", "0\n0,1,4\n\n1,4\n"),
+        std::make_tuple("r8", std::string({0x00, 0x04, 0x00, 0x00, 0x02, 0x00, 0x05, 0x01, 0x02, 0x00}))};
+
+    for (const auto& [in_format, in_data] : detection_data) {
+        for (const auto& [out_format, out_data] : detection_data) {
+            ASSERT_EQ(
+                run_captured_stim_main(
+                    {"convert",
+                     "--in_format",
+                     in_format.c_str(),
+                     "--out_format",
+                     out_format.c_str(),
+                     "--circuit",
+                     tmp.path.data(),
+                     "--types=DL"},
+                    in_data),
+                out_data);
+        }
+    }
 }
-
-INSTANTIATE_TEST_SUITE_P(
-    ConvertMeasurements,
-    ConvertTest,
-    testing::Combine(
-        testing::Values("M"), testing::ValuesIn(measurement_parameters), testing::ValuesIn(measurement_parameters)),
-    GenerateTestParameterName<ConvertTest::ParamType>);
-
-INSTANTIATE_TEST_SUITE_P(
-    ConvertDetections,
-    ConvertTest,
-    testing::Combine(
-        testing::Values("DL"), testing::ValuesIn(detection_parameters), testing::ValuesIn(detection_parameters)),
-    GenerateTestParameterName<ConvertTest::ParamType>);
 
 TEST(command_convert, convert_invalid_types) {
     RaiiTempNamedFile tmp("");
