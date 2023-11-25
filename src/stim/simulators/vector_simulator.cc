f6133953ba7f98b44b59b012a9266087c9c931f8
Craig Gidney <craiggidney@google.com>
2023-11-24 16:04:07 -0800

    format

--- src/stim/simulators/vector_simulator.cc
+++ src/stim/simulators/vector_simulator.cc
@@ -75,7 +75,8 @@ void VectorSimulator::apply(GateType gate, size_t qubit) {
     try {
         apply(GATE_DATA[gate].unitary(), {qubit});
     } catch (const std::out_of_range &) {
-        throw std::out_of_range("Single qubit gate isn't supported by VectorSimulator: " + std::string(GATE_DATA[gate].name));
+        throw std::out_of_range(
+            "Single qubit gate isn't supported by VectorSimulator: " + std::string(GATE_DATA[gate].name));
     }
 }
 
@@ -83,7 +84,8 @@ void VectorSimulator::apply(GateType gate, size_t qubit1, size_t qubit2) {
     try {
         apply(GATE_DATA[gate].unitary(), {qubit1, qubit2});
     } catch (const std::out_of_range &) {
-        throw std::out_of_range("Two qubit gate isn't supported by VectorSimulator: " + std::string(GATE_DATA[gate].name));
+        throw std::out_of_range(
+            "Two qubit gate isn't supported by VectorSimulator: " + std::string(GATE_DATA[gate].name));
     }
 }
 
