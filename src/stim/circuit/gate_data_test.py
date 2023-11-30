17ee69c4b9a1323ec50711e6881bf162a10afeb7
Craig Gidney <craiggidney@google.com>
2023-11-29 17:23:24 -0800

    Add `stim.GateData.{inverse,generalized_inverse}`

--- src/stim/circuit/gate_data_test.py
+++ src/stim/circuit/gate_data_test.py
@@ -1,3 +1,4 @@
+import numpy as np
 import stim
 
 
@@ -53,3 +54,23 @@ stim.GateData {
 def test_gate_data_repr():
     val = stim.GateData('MPP')
     assert eval(repr(val), {"stim": stim}) == val
+
+
+def test_gate_data_inverse():
+    for v in stim.gate_data().values():
+        assert v.is_unitary == (v.inverse is not None)
+        if v.is_unitary:
+            assert np.allclose(v.unitary_matrix.conj().T, v.inverse.unitary_matrix, atol=1e-6)
+            assert v.inverse == v.generalized_inverse
+
+    assert stim.gate_data('H').inverse == stim.gate_data('H')
+    assert stim.gate_data('S').inverse == stim.gate_data('S_DAG')
+    assert stim.gate_data('M').inverse is None
+    assert stim.gate_data('CXSWAP').inverse == stim.gate_data('SWAPCX')
+
+    assert stim.gate_data('S').generalized_inverse == stim.gate_data('S_DAG')
+    assert stim.gate_data('M').generalized_inverse == stim.gate_data('M')
+    assert stim.gate_data('R').generalized_inverse == stim.gate_data('MR')
+    assert stim.gate_data('MR').generalized_inverse == stim.gate_data('MR')
+    assert stim.gate_data('MPP').generalized_inverse == stim.gate_data('MPP')
+    assert stim.gate_data('ELSE_CORRELATED_ERROR').generalized_inverse == stim.gate_data('ELSE_CORRELATED_ERROR')
