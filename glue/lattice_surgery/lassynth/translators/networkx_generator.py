cdda7501a92b9b258035a1b352bd0edef0847d2c
Daniel Bochen Tan <33230390+qu-tan-um@users.noreply.github.com>
2024-05-17 22:35:54 -0700

    updates for LaSSynth (#770)
    - Cleaned up code and added comments.
    - Adapt to finalized naming in the paper. README and demo notebook also
    updated.
    - Fixed issue of missing faces when generated GLTF is opened in other
    software. Now every face is actually two back-to-back sides both
    providing color. User can also opt in to add the IJK axes in the gltf.
    - Unified a lot of code in the textfig, zigxag, and graph generator to
    Stim ZX. These three now are all based on `ZXGridGraph`.
    
    ---------
    
    Co-authored-by: Daniel Tan <bj.tanbochen@gmail.com>

---
+++ glue/lattice_surgery/lassynth/translators/networkx_generator.py
@@ -0,0 +1,53 @@
+"""generate a annotated networkx.Graph corresponding to the LaS."""
+
+import networkx
+from lassynth.translators import ZXGridGraph
+import stimzx
+from typing import Mapping, Any
+
+
+def networkx_generator(lasre: Mapping[str, Any]) -> networkx.Graph:
+    n_i, n_j, n_k = lasre["n_i"], lasre["n_j"], lasre["n_k"]
+    port_cubes = lasre["port_cubes"]
+    zxgridgraph = ZXGridGraph(lasre)
+    edges = zxgridgraph.edges
+    nodes = zxgridgraph.nodes
+
+    zx_nx_graph = networkx.Graph()
+    type_to_str = {"X": "X", "Z": "Z", "Pi": "in", "Po": "out", "I": "X"}
+    cnt = 0
+    for (i, j, k) in port_cubes:
+        node = nodes[i][j][k]
+        zx_nx_graph.add_node(cnt, value=stimzx.ZxType(type_to_str[node.type]))
+        node.node_id = cnt
+        cnt += 1
+
+    for i in range(n_i + 1):
+        for j in range(n_j + 1):
+            for k in range(n_k + 1):
+                node = nodes[i][j][k]
+                if node.type not in ["N", "Po", "Pi"]:
+                    zx_nx_graph.add_node(cnt,
+                                         value=stimzx.ZxType(
+                                             type_to_str[node.type]))
+                    node.node_id = cnt
+                    cnt += 1
+                if node.y_tail_minus:
+                    zx_nx_graph.add_node(cnt, value=stimzx.ZxType("Z", 1))
+                    zx_nx_graph.add_edge(node.node_id, cnt)
+                    cnt += 1
+                if node.y_tail_plus:
+                    zx_nx_graph.add_node(cnt, value=stimzx.ZxType("Z", 3))
+                    zx_nx_graph.add_edge(node.node_id, cnt)
+                    cnt += 1
+
+    for edge in edges:
+        if edge.type != "h":
+            zx_nx_graph.add_edge(edge.node0.node_id, edge.node1.node_id)
+        else:
+            zx_nx_graph.add_node(cnt, value=stimzx.ZxType("H"))
+            zx_nx_graph.add_edge(cnt, edge.node0.node_id)
+            zx_nx_graph.add_edge(cnt, edge.node1.node_id)
+            cnt += 1
+
+    return zx_nx_graph
--- glue/lattice_surgery/setup.py
+++ glue/lattice_surgery/setup.py
@@ -1,19 +1,19 @@
 from setuptools import find_packages, setup
 
 with open('README.md', encoding='UTF-8') as f:
-  long_description = f.read()
+    long_description = f.read()
 
 __version__ = '0.1.0'
 
 setup(
-    name='olssco',
+    name='LaSsynth',
     version=__version__,
-    author='Daniel Bochen Tan',
-    author_email='tbcdebug@outlook.com',
-    url='https://github.com/quantumlib/stim/',
+    author='',
+    author_email='',
+    url='',
     license='Apache 2',
     packages=find_packages(),
-    description='A Compiler for Lattice Surgery Subroutines',
+    description='Lattice Surgery Subroutine Synthesizer',
     long_description=long_description,
     long_description_content_type='text/markdown',
     python_requires='>=3.6.0',
@@ -22,6 +22,7 @@ setup(
         'z3-solver==4.12.1.0',
         'stim',
         'networkx',
+        'ipykernel',
     ],
     tests_require=['pytest', 'python3-distutils'],
 )
