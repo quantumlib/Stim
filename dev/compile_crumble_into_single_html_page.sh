0f98f542d520e2c616bd4a6be74e4e2922e9a786
Craig Gidney <craiggidney@google.com>
2024-06-03 13:14:19 -0700

    Regen

--- dev/compile_crumble_into_single_html_page.sh
+++ dev/compile_crumble_into_single_html_page.sh
@@ -7,7 +7,7 @@ cd "$(git rev-parse --show-toplevel)"
 cat glue/crumble/crumble.html | grep -v "^<script";
 echo "<script>";
 # HACK: this temp file is to work around https://github.com/rollup/rollup/issues/5097
-rollup glue/crumble/main.js > tmp_crumble.tmp
+rollup glue/crumble/main.js --silent > tmp_crumble.tmp
 uglifyjs -c -m --mangle-props --toplevel < tmp_crumble.tmp;
 rm tmp_crumble.tmp
 echo "</script>";
