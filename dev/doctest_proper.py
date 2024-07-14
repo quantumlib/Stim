1a54da6f388d2c8cfb5df1cfe42966169f661c31
Craig Gidney <craiggidney@google.com>
2024-07-13 22:55:54 -0700

    Warn less

--- dev/doctest_proper.py
+++ dev/doctest_proper.py
@@ -90,6 +90,8 @@ def main():
         out = {}
         gen(obj=module, fullname=module_name, out=out)
         for k, v in out.items():
+            if v.__doc__ is None:
+                continue
             v = v.__doc__.lower()
             if '\n' in v.strip() and 'examples:' not in v and 'example:' not in v and '[deprecated]' not in v:
                 if k.split('.')[-1] not in ['__next__', '__iter__', '__init_subclass__', '__module__', '__eq__', '__ne__', '__str__', '__repr__']:
