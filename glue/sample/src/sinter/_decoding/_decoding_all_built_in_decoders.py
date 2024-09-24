df993022181bed38b70befdf9acb22362b9d6e4f
Craig Gidney <craiggidney@google.com>
2024-09-23 23:58:29 -0700

    Add `"hypergraph_union_find"` and `"mw_parity_factor"` decoders to sinter (#837)
    (This PR is just a merge-conflict-resolved version of
    https://github.com/quantumlib/Stim/pull/815)
    
    > This is a simple adaptation of the `mwpf' python package to sinter.
    Just copied the `_decoding_fusion.py` and modify it accordingly to let
    the decoder consider hyperedges. Tested using the
    `getting_started.ipynb` and they run decoding without a problem.
    
    > I haven't added any test cases for this, since I didn't find a test
    case for `fusion_blossom` decoder as well. Please let me know where
    should I put a basic test case for the new decoders!
    
    Editor's note: The existing unit tests automatically iterate over the defined decoders and run them through some basic sanity checks.
    
    Authored by: Yue Wu <yue.wu@yale.edu>

--- glue/sample/src/sinter/_decoding/_decoding_all_built_in_decoders.py
+++ glue/sample/src/sinter/_decoding/_decoding_all_built_in_decoders.py
@@ -7,11 +7,16 @@ from sinter._decoding._decoding_pymatching import PyMatchingDecoder
 from sinter._decoding._decoding_vacuous import VacuousDecoder
 from sinter._decoding._perfectionist_sampler import PerfectionistSampler
 from sinter._decoding._sampler import Sampler
+from sinter._decoding._decoding_mwpf import HyperUFDecoder, MwpfDecoder
 
 BUILT_IN_DECODERS: Dict[str, Decoder] = {
     'vacuous': VacuousDecoder(),
     'pymatching': PyMatchingDecoder(),
     'fusion_blossom': FusionBlossomDecoder(),
+    # an implementation of (weighted) hypergraph UF decoder (https://arxiv.org/abs/2103.08049)
+    'hypergraph_union_find': HyperUFDecoder(),
+    # Minimum-Weight Parity Factor using similar primal-dual method the blossom algorithm (https://pypi.org/project/mwpf/)
+    'mw_parity_factor': MwpfDecoder(),
 }
 
 BUILT_IN_SAMPLERS: Dict[str, Union[Decoder, Sampler]] = {
