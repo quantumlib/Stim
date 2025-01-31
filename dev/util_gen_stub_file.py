61719e493b08a630d20358b9bd2a816daf5efb91
Craig Gidney <craiggidney@google.com>
2025-01-30 21:29:58 -0800

    cover

--- dev/util_gen_stub_file.py
+++ dev/util_gen_stub_file.py
@@ -102,6 +102,7 @@ def splay_signature(sig: str) -> List[str]:
     sig = sig.replace('dict[', 'Dict[')
     sig = sig.replace('tuple[', 'Tuple[')
     sig = sig.replace('set[', 'Set[')
+    sig = sig.replace('pathlib._local.Path', 'pathlib.Path')
 
     assert sig.startswith('def')
     out = []
