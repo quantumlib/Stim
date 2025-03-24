a0ecfc81cefdb5c91418f1619a276030df9106d6
Craig Gidney <craiggidney@google.com>
2025-03-24 13:19:31 -0700

    x
    Signed-off-by: Craig Gidney <craiggidney@google.com>

--- src/stim/stabilizers/clifford_string.h
+++ src/stim/stabilizers/clifford_string.h
@@ -1,194 +1,181 @@
-///*
-// * Copyright 2021 Google LLC
-// *
-// * Licensed under the Apache License, Version 2.0 (the "License");
-// * you may not use this file except in compliance with the License.
-// * You may obtain a copy of the License at
-// *
-// *      http://www.apache.org/licenses/LICENSE-2.0
-// *
-// * Unless required by applicable law or agreed to in writing, software
-// * distributed under the License is distributed on an "AS IS" BASIS,
-// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// * See the License for the specific language governing permissions and
-// * limitations under the License.
-// */
-//
-//#ifndef _STIM_STABILIZERS_CLIFFORD_STRING_H
-//#define _STIM_STABILIZERS_CLIFFORD_STRING_H
-//
-//#include "stim/mem/simd_bits.h"
-//#include "stim/gates/gates.h"
-//
-//namespace stim {
-//
-//template <size_t W>
-//struct CliffordWord {
-//    bitword<W> x_signs;
-//    bitword<W> z_signs;
-//    bitword<W> inv_x2x;
-//    bitword<W> x2z;
-//    bitword<W> z2x;
-//    bitword<W> inv_z2z;
-//};
-//
-//template <size_t W>
-//inline CliffordWord<W> operator*(const CliffordWord<W> &lhs, const CliffordWord<W> &rhs) const {
-//    CliffordWord<W> result;
-//    result.inv_x2x = (lhs.inv_x2x | rhs.inv_x2x) ^ (lhs.z2x & rhs.x2z);
-//    result.x2z = rhs.inv_x2x.andnot(lhs.x2z) ^ lhs.inv_z2z.andnot(rhs.x2z);
-//    result.z2x = lhs.inv_x2x.andnot(rhs.z2x) ^ rhs.z2z.andnot(lhs.z2x);
-//    result.inv_z2z = (lhs.x2z & rhs.z2x) ^ (lhs.inv_z2z | rhs.inv_z2z);
-//    simd_word<W> rhs_x2y = rhs.inv_x2x.andnot(rhs.x2z);
-//    simd_word<W> rhs_z2y = rhs.z2z.andnot(rhs.z2x);
-//    simd_word<W> dy = (lhs.x2z & lhs.z2x) ^ lhs.inv_x2x ^ lhs.z2x ^ lhs.x2z ^ lhs.inv_z2z;
-//    result.x_signs = (
-//        rhs.x_signs
-//        ^ rhs.inv_x2x.andnot(lhs.x_signs)
-//        ^ (rhs_x2y & dy)
-//        ^ (rhs.x2z & lhs.z_signs)
-//    );
-//    result.z_signs = (
-//        rhs.z_signs
-//        ^ (rhs.z2x & lhs.x_signs)
-//        ^ (rhs_z2y & dy)
-//        ^ rhs.inv_z2z.andnot(lhs.z_signs)
-//    );
-//    return result
-//}
-//
-//template <size_t W>
-//struct CliffordString {
-//    size_t num_qubits;
-//    size_t num_words;
-//    CliffordWord<W> *buf;
-//
-//    CliffordString(size_t num_qubits, bool do_not_initialize = false)
-//        : num_qubits(num_qubits),
-//          num_words(((num_qubits + W - 1) / W) * W),
-//          buf(nullptr) {
-//        if (num_words) {
-//            buf = bit_word<W>::aligned_malloc(num_words * (W / 8));
-//        }
-//    }
-//    static inline CliffordString<W> uninitialized(size_t num_qubits) {
-//        return CliffordString(num_qubits);
-//    }
-//    static inline CliffordString<W> identities(size_t num_qubits) {
-//        CliffordString<W> result(num_qubits);
-//        memset(result.buf, 0, num_words * (W / 8));
-//        return result;
-//    }
-//
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef _STIM_STABILIZERS_CLIFFORD_STRING_H
+#define _STIM_STABILIZERS_CLIFFORD_STRING_H
+
+#include "stim/mem/simd_bits.h"
+#include "stim/gates/gates.h"
+
+namespace stim {
+
+template <size_t W>
+struct CliffordWord {
+    bitword<W> x_signs;
+    bitword<W> z_signs;
+    bitword<W> inv_x2x;
+    bitword<W> x2z;
+    bitword<W> z2x;
+    bitword<W> inv_z2z;
+};
+
+template <size_t W>
+inline CliffordWord<W> operator*(const CliffordWord<W> &lhs, const CliffordWord<W> &rhs) {
+    CliffordWord<W> result;
+    result.inv_x2x = (lhs.inv_x2x | rhs.inv_x2x) ^ (lhs.z2x & rhs.x2z);
+    result.x2z = rhs.inv_x2x.andnot(lhs.x2z) ^ lhs.inv_z2z.andnot(rhs.x2z);
+    result.z2x = lhs.inv_x2x.andnot(rhs.z2x) ^ rhs.z2z.andnot(lhs.z2x);
+    result.inv_z2z = (lhs.x2z & rhs.z2x) ^ (lhs.inv_z2z | rhs.inv_z2z);
+    simd_word<W> rhs_x2y = rhs.inv_x2x.andnot(rhs.x2z);
+    simd_word<W> rhs_z2y = rhs.z2z.andnot(rhs.z2x);
+    simd_word<W> dy = (lhs.x2z & lhs.z2x) ^ lhs.inv_x2x ^ lhs.z2x ^ lhs.x2z ^ lhs.inv_z2z;
+    result.x_signs = (
+        rhs.x_signs
+        ^ rhs.inv_x2x.andnot(lhs.x_signs)
+        ^ (rhs_x2y & dy)
+        ^ (rhs.x2z & lhs.z_signs)
+    );
+    result.z_signs = (
+        rhs.z_signs
+        ^ (rhs.z2x & lhs.x_signs)
+        ^ (rhs_z2y & dy)
+        ^ rhs.inv_z2z.andnot(lhs.z_signs)
+    );
+    return result;
+}
+
+template <size_t W>
+struct CliffordString {
+    size_t num_qubits;
+    simd_bits<W> x_signs;
+    simd_bits<W> z_signs;
+    simd_bits<W> inv_x2x;
+    simd_bits<W> x2z;
+    simd_bits<W> z2x;
+    simd_bits<W> inv_z2z;
+
+    CliffordString(size_t num_qubits)
+        : num_qubits(num_qubits),
+          x_signs(num_qubits),
+          z_signs(num_qubits),
+          inv_x2x(num_qubits),
+          x2z(num_qubits),
+          z2x(num_qubits),
+          inv_z2z(num_qubits) {
+    }
+
+    inline CliffordWord<W> word_at(size_t k) const {
+        return CliffordWord<W>{
+            x_signs.ptr_simd[k],
+            z_signs.ptr_simd[k],
+            inv_x2x.ptr_simd[k],
+            x2z.ptr_simd[k],
+            z2x.ptr_simd[k],
+            inv_z2z.ptr_simd[k],
+        };
+    }
+    inline void set_word_at(size_t k, CliffordWord<W> new_value) const {
+        x_signs.ptr_simd[k] = new_value.x_signs;
+        z_signs.ptr_simd[k] = new_value.z_signs;
+        inv_x2x.ptr_simd[k] = new_value.inv_x2x;
+        x2z.ptr_simd[k] = new_value.x2z;
+        z2x.ptr_simd[k] = new_value.z2x;
+        inv_z2z.ptr_simd[k] = new_value.inv_z2z;
+    }
+
 //    stim::GateType gate_at(size_t index) const {
 //        return table[flat_value];
 //    }
-//    void set_gate_at(size_t index, GateType gate) const {
-//        const auto &flows = stim::GATE_DATA[gate].flow_data;
-//        std::string_view tx = flows[0];
-//        std::string_view tz = flows[1];
-//        bool inv_x2x = tx[0] == 'X' || tx[0] == 'Y';
-//        bool x2z = tx[0] == 'Z' || tx[0] == 'Y';
-//        bool x_sign = tx[0] == '-';
-//
-//        bool inv_z2x = tz[0] == 'X' || tz[0] == 'Y';
-//        bool z2z = tz[0] == 'Z' || tz[0] == 'Y';
-//        bool z_sign = tz[0] == '-';
-//
-//
-//    }
-//
-//    CliffordString(const CliffordString &other)
-//        : num_qubits(other.num_qubits),
-//          num_words(other.num_words),
-//          buf(nullptr) {
-//        buf = bit_word<W>::aligned_malloc(num_words * (W / 8));
-//        memcpy(buf, other.buf, num_words * (W / 8));
-//    }
-//    CliffordString(CliffordString &&other)
-//        : num_qubits(other.num_qubits),
-//          num_words(other.num_words),
-//          buf(other.buf) {
-//        other.buf = nullptr;
-//        other.num_qubits = 0;
-//        other.num_words = 0;
-//    }
-//    ~CliffordString() {
-//        if (buf != nullptr) {
-//            bit_word<W>::aligned_free(buf);
-//            buf = nullptr;
-//        }
-//        num_qubits = 0;
-//        num_words = 0;
-//    }
-//    CliffordString &operator=(const CliffordString &other) {
-//        if (num_words != other.num_words && buf != nullptr) {
-//            bit_word<W>::aligned_free(buf);
-//            buf = nullptr;
-//            num_words = 0;
-//            num_qubits = 0;
-//        }
-//        if (buf == nullptr && num_words > 0) {
-//            buf = bit_word<W>::aligned_malloc(other.num_words * (W / 8));
-//        }
-//        num_words = other.num_words;
-//        num_qubits = other.num_qubits;
-//        memcpy(buf, other.buf, num_words * (W / 8));
-//        return *this;
-//    }
-//    CliffordString &operator=(CliffordString &&other) {
-//        num_words = other.num_words;
-//        num_qubits = other.num_qubits;
-//        if (buf != nullptr) {
-//            bit_word<W>::aligned_free(buf);
-//        }
-//        buf = other.buf;
-//        other.buf = nullptr;
-//        other.num_qubits = 0;
-//        other.num_words = 0;
-//        return *this;
-//    }
-//
-//    CliffordString &operator*=(const CliffordString &rhs) {
-//        if (num_words < rhs.num_words) {
-//            throw std::invalid_argument("Can't inplace-multiply by a larger Clifford string.");
-//        }
-//        for (size_t k = 0; k < rhs.num_words; k++) {
-//            buf[k] = buf[k] * rhs.buf[k];
-//        }
-//        return *this;
-//    }
-//    CliffordString &inplace_left_mul_by(const CliffordString &lhs) {
-//        if (num_words < lhs.num_words) {
-//            throw std::invalid_argument("Can't inplace-multiply by a larger Clifford string.");
-//        }
-//        for (size_t k = 0; k < num_words; k++) {
-//            buf[k] = lhs.buf[k] * buf[k];
-//        }
-//        return *this;
-//    }
-//    CliffordString operator*(const CliffordString &rhs) const {
-//        CliffordString<W> result = CliffordString<W>::uninitialized(std::max(num_qubits, rhs.num_qubits));
-//        size_t min_words = std::min(num_words, rhs.num_words);
-//        for (size_t k = 0; k < min_size; k++) {
-//            result.buf[k] = buf[k] * rhs.buf[k];
-//        }
-//        for (size_t k = min_size; k < num_words; k++) {
-//            result.buf[k] = buf[k];
-//        }
-//        for (size_t k = min_size; k < rhs.num_words; k++) {
-//            result.buf[k] = rhs.buf[k];
-//        }
-//        return *this;
-//    }
-//};
-//
-///// Writes a string describing the given Clifford string to an output stream.
+
+    void set_gate_at(size_t index, GateType gate) const {
+        const auto &flows = stim::GATE_DATA[gate].flow_data;
+        std::string_view tx = flows[0];
+        std::string_view tz = flows[1];
+        bool new_inv_x2x = !(tx[0] == 'X' || tx[0] == 'Y');
+        bool new_x2z = tx[0] == 'Z' || tx[0] == 'Y';
+        bool new_x_sign = tx[0] == '-';
+
+        bool new_z2x = tz[0] == 'X' || tz[0] == 'Y';
+        bool new_inv_z2z = !(tz[0] == 'Z' || tz[0] == 'Y');
+        bool new_z_sign = tz[0] == '-';
+
+        x_signs[index] = new_x_sign;
+        z_signs[index] = new_z_sign;
+        inv_x2x[index] = new_inv_x2x;
+        x2z[index] = new_x2z;
+        z2x[index] = new_z2x;
+        inv_z2z[index] = new_inv_z2z;
+    }
+
+    CliffordString &operator*=(const CliffordString &rhs) {
+        if (num_qubits < rhs.num_qubits) {
+            throw std::invalid_argument("Can't inplace-multiply by a larger Clifford string.");
+        }
+        for (size_t k = 0; k < rhs.num_words; k++) {
+            auto lhs_w = word_at(k);
+            auto rhs_w = rhs.word_at(k);
+            set_word_at(k, lhs_w * rhs_w);
+        }
+        return *this;
+    }
+    CliffordString &inplace_left_mul_by(const CliffordString &lhs) {
+        if (num_qubits < lhs.num_qubits) {
+            throw std::invalid_argument("Can't inplace-multiply by a larger Clifford string.");
+        }
+        for (size_t k = 0; k < x_signs.num_simd_words; k++) {
+            auto lhs_w = lhs.word_at(k);
+            auto rhs_w = word_at(k);
+            set_word_at(k, lhs_w * rhs_w);
+        }
+        return *this;
+    }
+    CliffordString operator*(const CliffordString &rhs) const {
+        CliffordString<W> result = CliffordString<W>(std::max(num_qubits, rhs.num_qubits));
+        size_t min_words = std::min(x_signs.num_simd_words, rhs.x_signs.num_words);
+        for (size_t k = 0; k < min_words; k++) {
+            auto lhs_w = word_at(k);
+            auto rhs_w = rhs.word_at(k);
+            result.set_word_at(k, lhs_w * rhs_w);
+        }
+
+        // The longer string copies its tail into the result.
+        size_t min_qubits = std::min(num_qubits, rhs.num_qubits);
+        for (size_t q = min_qubits; q < num_qubits; q++) {
+            result.x_signs[q] = x_signs[q];
+            result.z_signs[q] = z_signs[q];
+            result.inv_x2x[q] = inv_x2x[q];
+            result.x2z[q] = x2z[q];
+            result.z2x[q] = z2x[q];
+            result.inv_z2z[q] = inv_z2z[q];
+        }
+        for (size_t q = min_qubits; q < rhs.num_qubits; q++) {
+            result.x_signs[q] = rhs.x_signs[q];
+            result.z_signs[q] = rhs.z_signs[q];
+            result.inv_x2x[q] = rhs.inv_x2x[q];
+            result.x2z[q] = rhs.x2z[q];
+            result.z2x[q] = rhs.z2x[q];
+            result.inv_z2z[q] = rhs.inv_z2z[q];
+        }
+        return *this;
+    }
+};
+
 //template <size_t W>
-//std::ostream &operator<<(std::ostream &out, const PauliString<W> &ps);
-//
-//}  // namespace stim
-//
-//#include "stim/stabilizers/pauli_string.inl"
-//
-//#endif
+//std::ostream &operator<<(std::ostream &out, const CliffordString<W> &v);
+
+}  // namespace stim
+
+#endif
--- src/stim/stabilizers/clifford_string.test.cc
+++ src/stim/stabilizers/clifford_string.test.cc
@@ -6,7 +6,35 @@
 using namespace stim;
 
 TEST_EACH_WORD_SIZE_W(clifford_string, mul, {
-    CliffordString<W> p1 = CliffordString<W>::uninitialized(5);
-    FlexPauliString p2 = FlexPauliString::from_text("i__Z");
-    ASSERT_EQ(p1 * p2, FlexPauliString::from_text("-XY_"));
-}
+    std::vector<Gate> single_qubit_gates;
+    for (size_t g = 0; g < NUM_DEFINED_GATES; g++) {
+        Gate gate = GATE_DATA[(GateType)g];
+        if ((gate.flags & GateFlags::GATE_IS_SINGLE_QUBIT_GATE) && (gate.flags & GateFlags::GATE_IS_UNITARY)) {
+            single_qubit_gates.push_back(gate);
+        }
+    }
+    ASSERT_EQ(single_qubit_gates.size(), 24);
+
+    std::map<std::string, GateType> t2g;
+    for (const auto &g : single_qubit_gates) {
+        t2g[g.tableau<W>().str()] = g.id;
+    }
+
+    CliffordString<W> p1 = CliffordString<W>::uninitialized(24 * 24);
+    CliffordString<W> p2 = CliffordString<W>::uninitialized(24 * 24);
+    CliffordString<W> p12 = CliffordString<W>::uninitialized(24 * 24);
+    for (size_t k1 = 0; k1 < 24; k1++) {
+        for (size_t k2 = 0; k2 < 24; k2++) {
+            size_t k = k1 * 24 + k2;
+            Gate g1 = single_qubit_gates[k1];
+            Gate g2 = single_qubit_gates[k2];
+            p1.set_gate_at(k, g1.id);
+            p2.set_gate_at(k, g2.id);
+            auto t1 = g1.tableau<W>();
+            auto t2 = g2.tableau<W>();
+            auto t3 = t2.then(t1);
+            auto g3 = t2g[t3.str()];
+            p12.set_gate_at(k, g3);
+        }
+    }
+})
