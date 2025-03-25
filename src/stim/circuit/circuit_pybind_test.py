988915b7652be908465a9871cd56a852c60c6115
Craig Gidney <craiggidney@google.com>
2025-03-24 18:34:25 -0700

    Add `stim.Circuit.reference_detector_and_observable_signs` (#914)
    Fixes https://github.com/quantumlib/Stim/issues/841

--- src/stim/circuit/circuit_pybind_test.py
+++ src/stim/circuit/circuit_pybind_test.py
@@ -2360,3 +2360,38 @@ def test_without_tags():
     assert circuit.without_tags() == stim.Circuit("""
         H 5
     """)
+
+
+def test_reference_detector_and_observable_signs():
+    det, obs = stim.Circuit("""
+        X 1
+        M 0 1
+        DETECTOR rec[-1]
+        DETECTOR rec[-2]
+        OBSERVABLE_INCLUDE(3) rec[-1] rec[-2]
+    """).reference_detector_and_observable_signs()
+    assert det.dtype == np.bool_
+    assert obs.dtype == np.bool_
+    np.testing.assert_array_equal(det, [True, False])
+    np.testing.assert_array_equal(obs, [False, False, False, True])
+
+    det, obs = stim.Circuit("""
+        X 1
+        M 0 1
+        DETECTOR rec[-1]
+        DETECTOR rec[-2]
+        OBSERVABLE_INCLUDE(3) rec[-1] rec[-2]
+    """).reference_detector_and_observable_signs(bit_packed=True)
+    assert det.dtype == np.uint8
+    assert obs.dtype == np.uint8
+    np.testing.assert_array_equal(det, [0b01])
+    np.testing.assert_array_equal(obs, [0b1000])
+
+    circuit = stim.Circuit.generated("surface_code:rotated_memory_x", rounds=3, distance=3)
+    det, obs = circuit.reference_detector_and_observable_signs(bit_packed=True)
+    assert det.dtype == np.uint8
+    assert obs.dtype == np.uint8
+    assert not np.any(det)
+    assert not np.any(obs)
+    assert len(det) == (circuit.num_detectors + 7) // 8
+    assert len(obs) == 1
