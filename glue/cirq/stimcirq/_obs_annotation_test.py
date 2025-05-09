13d2c5f9736cc3c259812bf85fe58edc42557d07
Alexandre Bourassa <abourassa@google.com>
2025-05-09 14:47:19 -0700

    mod

--- glue/cirq/stimcirq/_obs_annotation_test.py
+++ glue/cirq/stimcirq/_obs_annotation_test.py
@@ -216,4 +216,5 @@ def test_json_backwards_compat_exact():
     # With pauli_keys
     raw = stimcirq.CumulativeObservableAnnotation(parity_keys=['z'], relative_keys=[-2], observable_index=5, pauli_keys=["X0", "Y1", "Z2"])
     packed_v2 ='{\n  "cirq_type": "CumulativeObservableAnnotation",\n  "parity_keys": [\n    "z"\n  ],\n  "observable_index": 5,\n  "pauli_keys": [\n    "X0",\n    "Y1",\n    "Z2"\n  ],\n  "relative_keys": [\n    -2\n  ]\n}'
+    assert cirq.read_json(json_text=packed_v2, resolvers=[*cirq.DEFAULT_RESOLVERS, stimcirq.JSON_RESOLVER]) == raw
     assert cirq.to_json(raw) == packed_v2
