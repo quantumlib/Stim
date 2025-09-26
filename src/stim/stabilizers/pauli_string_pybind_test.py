81777432ae908b2f1659a04d67661c5473fe6814
Iftach Yakar <iftachyakar@gmail.com>
2025-09-26 15:17:01 +0300

    Added an edge case test

--- src/stim/stabilizers/pauli_string_pybind_test.py
+++ src/stim/stabilizers/pauli_string_pybind_test.py
@@ -977,6 +977,7 @@ def test_constructor_from_dict():
     assert stim.PauliString({0: [9]}) == stim.PauliString(10)
     assert stim.PauliString({"_": [9]}) == stim.PauliString(10)
 
+    # Acceptable collisions:
     assert stim.PauliString({"X": [2], "I": [2]}) == stim.PauliString("__X") # Trivial Pauli should not cause a conflict
     assert stim.PauliString({"X": [2], 1: [2]}) == stim.PauliString("__X") # Same Pauli should not cause conflict between int/str
     assert stim.PauliString({"X": [2], "I": [0,1,2,3], "Z": [0], 0: [0,1,2,3], "Y": [1], "_": [0,1,2,3]}) == stim.PauliString("ZYX_") # A more complex example
@@ -1020,6 +1021,9 @@ def test_constructor_from_dict_errors():
 
     with pytest.raises(ValueError, match="same qubit index"):
         stim.PauliString({"Z": [1], "Y": [4,1]}) # Different non-trivial Paulies can't use the same index
+
+    with pytest.raises(ValueError, match="same qubit index"):
+        stim.PauliString({"I": [0,1,4], "Z": [1], "Y": [4,1]}) # Different non-trivial Paulies can't use the same index
 
     with pytest.raises(ValueError, match="keys must all be ints"):
         stim.PauliString({(): []})
