9be57011dc74e62cb7d6d75d9402a18a466109b8
Craig Gidney <craiggidney@google.com>
2026-01-20 00:45:44 -0800

    moved

--- glue/python/src/stim_custom_setup/__init__.py
+++ glue/python/src/stim_custom_setup/__init__.py
@@ -160,6 +160,8 @@ def build_wheel(wheel_directory, config_settings=None, metadata_directory=None):
             '_detect_machine_architecture': ((), MUX_SOURCE_FILES),
             '_stim_polyfill': ((), RELEVANT_SOURCE_FILES),
             '_stim_sse2': (('-msse2', '-mno-avx2',), RELEVANT_SOURCE_FILES),
+            # NOTE: disabled until https://github.com/quantumlib/Stim/issues/432 is fixed
+            # '_stim_avx': (('-msse2', '-mavx2',), RELEVANT_SOURCE_FILES),
         }
         compile_commands = []
         link_commands = []
@@ -243,119 +245,3 @@ Tag: {get_wheel_tag()}
 
 if __name__ == '__main__':
     build_wheel(None)
-#
-# if platform.system().startswith('Win'):
-#     common_compile_args = [
-#         '/std:c++20',
-#         '/O2',
-#         f'/DVERSION_INFO={__version__}',
-#     ]
-#     arch_avx = ['/arch:AVX2']
-#     arch_sse = ['/arch:SSE2']
-#     arch_basic = []
-# else:
-#     common_compile_args = [
-#         '-std=c++20',
-#         '-fno-strict-aliasing',
-#         '-O3',
-#         '-g0',
-#         f'-DVERSION_INFO={__version__}',
-#     ]
-#     arch_avx = ['-mavx2']
-#     arch_sse = ['-msse2', '-mno-avx2']
-#     arch_basic = []
-#
-# class BuildExtParallel(build_ext):
-#     def finalize_options(self):
-#         super().finalize_options()
-#         if self.parallel is None:
-#             try:
-#                 self.parallel = multiprocessing.cpu_count()
-#             except (ImportError, NotImplementedError):
-#                 self.parallel = 1
-#         print(f"-- Building with {self.parallel} parallel jobs --")
-#
-# stim_detect_machine_architecture = Extension(
-#     'stim._detect_machine_architecture',
-#     sources=MUX_SOURCE_FILES,
-#     include_dirs=[pybind11.get_include(), "src"],
-#     language='c++',
-#     extra_compile_args=[
-#         *common_compile_args,
-#         *arch_basic,
-#     ],
-# )
-# stim_polyfill = Extension(
-#     'stim._stim_polyfill',
-#     sources=RELEVANT_SOURCE_FILES,
-#     include_dirs=[pybind11.get_include(), "src"],
-#     language='c++',
-#     extra_compile_args=[
-#         *common_compile_args,
-#         *arch_basic,
-#         '-DSTIM_PYBIND11_MODULE_NAME=_stim_polyfill',
-#     ],
-# )
-# stim_sse2 = Extension(
-#     'stim._stim_sse2',
-#     sources=RELEVANT_SOURCE_FILES,
-#     include_dirs=[pybind11.get_include(), "src"],
-#     language='c++',
-#     extra_compile_args=[
-#         *common_compile_args,
-#         *arch_sse,
-#         '-DSTIM_PYBIND11_MODULE_NAME=_stim_sse2',
-#     ],
-# )
-#
-# # NOTE: disabled until https://github.com/quantumlib/Stim/issues/432 is fixed
-# # stim_avx2 = Extension(
-# #     'stim._stim_avx2',
-# #     sources=RELEVANT_SOURCE_FILES,
-# #     include_dirs=[pybind11.get_include(), "src"],
-# #     language='c++',
-# #     extra_compile_args=[
-# #         *common_compile_args,
-# #         *arch_avx,
-# #         '-DSTIM_PYBIND11_MODULE_NAME=_stim_avx2',
-# #     ],
-# # )
-#
-# with open('glue/python/README.md', encoding='UTF-8') as f:
-#     long_description = f.read()
-#
-# def _get_extensions():
-#     archs=["x86", "i686", "i386", "amd64"]
-#     if any(_ext in platform.processor().lower() for _ext in archs):
-#         # NOTE: disabled until https://github.com/quantumlib/Stim/issues/432 is fixed
-#         # stim_avx2,
-#         return [stim_detect_machine_architecture, stim_polyfill,
-#                 # stim_avx2,
-#                 stim_sse2]
-#     else:
-#         return [stim_detect_machine_architecture, stim_polyfill]
-#
-# setup(
-#     name='stim',
-#     cmdclass={'build_ext': BuildExtParallel},
-#     version=__version__,
-#     author='Craig Gidney',
-#     author_email='craig.gidney@gmail.com',
-#     url='https://github.com/quantumlib/stim',
-#     license='Apache 2',
-#     description='A fast library for analyzing with quantum stabilizer circuits.',
-#     long_description=long_description,
-#     long_description_content_type='text/markdown',
-#     ext_modules=_get_extensions(),
-#     python_requires='>=3.6.0',
-#     packages=['stim'],
-#     package_dir={'stim': 'glue/python/src/stim'},
-#     package_data={'': [*HEADER_FILES, 'glue/python/src/stim/__init__.pyi', 'glue/python/README.md', 'pyproject.toml']},
-#     include_package_data=True,
-#     install_requires=['numpy'],
-#     entry_points={
-#         'console_scripts': ['stim=stim._main_argv:main_argv'],
-#     },
-#     # Needed on Windows to avoid the default `build` colliding with Bazel's `BUILD`.
-#     options={'build': {'build_base': 'python_build_stim'}},
-# )
