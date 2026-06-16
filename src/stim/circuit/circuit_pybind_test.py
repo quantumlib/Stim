23b04b1cadbfef287a21dc1d90c9ecc40186f5dc
Craig Gidney <craiggidney@google.com>
2026-06-15 22:37:29 -0700

    add print debug lines

--- src/stim/circuit/circuit_pybind_test.py
+++ src/stim/circuit/circuit_pybind_test.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 import io
 import pathlib
+import sys
 import tempfile
 from typing import cast
 
@@ -1497,14 +1498,18 @@ def test_num_ticks():
 
 
 def test_reference_sample():
+    print("TEST PROGRESSION: A", file=sys.stderr)
     circuit = stim.Circuit(
         """
         H 0
         CNOT 0 1
     """
     )
+    print("TEST PROGRESSION: B", file=sys.stderr)
     ref = circuit.reference_sample()
+    print("TEST PROGRESSION: C", file=sys.stderr)
     assert len(ref) == 0
+    print("TEST PROGRESSION: D", file=sys.stderr)
     circuit = stim.Circuit(
         """
         H 0 1
@@ -1512,16 +1517,27 @@ def test_reference_sample():
         MPP X0*X1 Y0*Y1 Z0*Z1
         """
     )
+    print("TEST PROGRESSION: E", file=sys.stderr)
     np.testing.assert_array_equal(circuit.reference_sample(), circuit.reference_sample())
+    print("TEST PROGRESSION: F", file=sys.stderr)
     assert np.sum(circuit.reference_sample()) % 2 == 1
+    print("TEST PROGRESSION: G", file=sys.stderr)
     circuit.append("X", (i for i in range(0, 100, 2)))
+    print("TEST PROGRESSION: H", file=sys.stderr)
     circuit.append("M", (i for i in range(100)))
+    print("TEST PROGRESSION: I", file=sys.stderr)
     ref_sample = circuit.reference_sample(bit_packed=True)
+    print("TEST PROGRESSION: J", file=sys.stderr)
     unpacked = np.unpackbits(ref_sample, bitorder="little")
+    print("TEST PROGRESSION: K", file=sys.stderr)
     expected = circuit.reference_sample(bit_packed=False)
+    print("TEST PROGRESSION: L", file=sys.stderr)
     expected_padded = np.zeros_like(unpacked)
+    print("TEST PROGRESSION: M", file=sys.stderr)
     expected_padded[:len(expected)] = expected
+    print("TEST PROGRESSION: N", file=sys.stderr)
     np.testing.assert_array_equal(unpacked, expected_padded)
+    print("TEST PROGRESSION: O", file=sys.stderr)
 
 
 def test_max_mix_depolarization_is_allowed_in_dem_conversion_without_args():
