f3930814984c898e1081b1be8ef5d9de1d5eb888
Craig Gidney <craiggidney@google.com>
2026-06-15 23:09:29 -0700

    repro

---
+++ mock.py
@@ -0,0 +1,25 @@
+import sys
+
+import stim
+import numpy as np
+
+
+def main():
+    circuit = stim.Circuit(
+        """
+        H 0 1
+        CX 0 2 1 3
+        MPP X0*X1 Y0*Y1 Z0*Z1
+        """
+    )
+    print("TEST PROGRESSION: A", file=sys.stderr)
+    aa = circuit.reference_sample()
+    print("TEST PROGRESSION: B", file=sys.stderr)
+    bb = circuit.reference_sample()
+    print("TEST PROGRESSION: C", file=sys.stderr)
+    np.testing.assert_array_equal(aa, bb)
+    print("TEST PROGRESSION: D", file=sys.stderr)
+
+
+if __name__ == '__main__':
+    main()
