29e07b43b8743344d74f14b999ecc00bb857f0d8
Craig Gidney <craiggidney@google.com>
2026-06-26 15:36:05 -0700

    drop march detection

--- glue/python/src/stim/__init__.py
+++ glue/python/src/stim/__init__.py
@@ -1,2 +1,1 @@
-from stim._stim_polyfill import _UNSTABLE_raw_format_data, __version__
 from stim._stim_polyfill import *
--- setup.py
+++ setup.py
@@ -18,12 +18,11 @@ import glob
 import pybind11
 
 ALL_SOURCE_FILES = glob.glob("src/**/*.cc", recursive=True)
-MUX_SOURCE_FILES = glob.glob("src/**/march.pybind.cc", recursive=True)
 TEST_FILES = glob.glob("src/**/*.test.cc", recursive=True)
 PERF_FILES = glob.glob("src/**/*.perf.cc", recursive=True)
 MAIN_FILES = glob.glob("src/**/main.cc", recursive=True)
 HEADER_FILES = glob.glob("src/**/*.h", recursive=True) + glob.glob("src/**/*.inl", recursive=True)
-RELEVANT_SOURCE_FILES = sorted(set(ALL_SOURCE_FILES) - set(TEST_FILES + PERF_FILES + MAIN_FILES + MUX_SOURCE_FILES))
+RELEVANT_SOURCE_FILES = sorted(set(ALL_SOURCE_FILES) - set(TEST_FILES + PERF_FILES + MAIN_FILES))
 
 __version__ = '1.17.dev0'
 
@@ -33,9 +32,6 @@ if platform.system().startswith('Win'):
         '/O2',
         f'/DVERSION_INFO={__version__}',
     ]
-    arch_avx = ['/arch:AVX2']
-    arch_sse = ['/arch:SSE2']
-    arch_basic = []
 else:
     common_compile_args = [
         '-std=c++20',
@@ -44,20 +40,7 @@ else:
         '-g0',
         f'-DVERSION_INFO={__version__}',
     ]
-    arch_avx = ['-mavx2']
-    arch_sse = ['-msse2', '-mno-avx2']
-    arch_basic = []
 
-stim_detect_machine_architecture = Extension(
-    'stim._detect_machine_architecture',
-    sources=MUX_SOURCE_FILES,
-    include_dirs=[pybind11.get_include(), "src"],
-    language='c++',
-    extra_compile_args=[
-        *common_compile_args,
-        *arch_basic,
-    ],
-)
 stim_polyfill = Extension(
     'stim._stim_polyfill',
     sources=RELEVANT_SOURCE_FILES,
@@ -65,22 +48,12 @@ stim_polyfill = Extension(
     language='c++',
     extra_compile_args=[
         *common_compile_args,
-        *arch_basic,
         '-DSTIM_PYBIND11_MODULE_NAME=_stim_polyfill',
     ],
 )
 
 with open('glue/python/README.md', encoding='UTF-8') as f:
     long_description = f.read()
-
-def _get_extensions():
-    archs=["x86", "i686", "i386", "amd64"]
-    if any(_ext in platform.processor().lower() for _ext in archs):
-        # NOTE: disabled until https://github.com/quantumlib/Stim/issues/432 is fixed
-        # stim_avx2,
-        return [stim_detect_machine_architecture, stim_polyfill]
-    else:
-        return [stim_detect_machine_architecture, stim_polyfill]
 
 setup(
     name='stim',
@@ -92,7 +65,7 @@ setup(
     description='A fast library for analyzing with quantum stabilizer circuits.',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    ext_modules=_get_extensions(),
+    ext_modules=[stim_polyfill],
     python_requires='>=3.6.0',
     packages=['stim'],
     package_dir={'stim': 'glue/python/src/stim'},
