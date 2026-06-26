a7153fb65fcda14b9b8d9da0586a76cccbaf3f35
Craig Gidney <craiggidney@google.com>
2026-06-26 15:59:09 -0700

    simpler test

--- src/stim/circuit/circuit_pybind_test.py
+++ src/stim/circuit/circuit_pybind_test.py
@@ -5,18 +5,6 @@ import numpy as np
 
 
 def test_reference_sample():
-    print("TEST PROGRESSION: A", file=sys.stderr)
-    circuit = stim.Circuit(
-        """
-        H 0
-        CNOT 0 1
-    """
-    )
-    print("TEST PROGRESSION: B", file=sys.stderr)
-    ref = circuit.reference_sample()
-    print("TEST PROGRESSION: C", file=sys.stderr)
-    assert len(ref) == 0
-    print("TEST PROGRESSION: D", file=sys.stderr)
     circuit = stim.Circuit(
         """
         H 0 1
@@ -24,7 +12,9 @@ def test_reference_sample():
         MPP X0*X1 Y0*Y1 Z0*Z1
         """
     )
+
     print("TEST PROGRESSION: E", file=sys.stderr)
-    np.testing.assert_array_equal(circuit.reference_sample(), circuit.reference_sample())
+    _ = circuit.reference_sample()
+    # np.testing.assert_array_equal(circuit.reference_sample(), circuit.reference_sample())
+
     print("TEST PROGRESSION: F", file=sys.stderr)
-    assert np.sum(circuit.reference_sample()) % 2 == 1
