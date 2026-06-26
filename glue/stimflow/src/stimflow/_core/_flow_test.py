b72b003ce45fb3170302970eee4fcc6165d01875
Craig Gidney <craiggidney@google.com>
2026-06-26 14:47:51 -0700

    remove all tests except the failing one

--- glue/stimflow/src/stimflow/_core/_flow_test.py
+++
@@ -1,7 +0,0 @@
-import stimflow
-
-
-def test_with_xz_flipped():
-    assert stimflow.Flow(start=stimflow.PauliMap({1: "X", 2: "Z"}), center=0).with_xz_flipped() == stimflow.Flow(
-        start=stimflow.PauliMap({1: "Z", 2: "X"}), center=0
-    )
--- glue/stimflow/src/stimflow/_layers/_layer_feedback_test.py
+++
@@ -1,7 +0,0 @@
-from stimflow._layers._layer_feedback import _basis_before_rotation
-
-
-def test_basis_before_rotation():
-    assert _basis_before_rotation("X", "C_ZYX") == "Y"
-    assert _basis_before_rotation("Y", "C_ZYX") == "Z"
-    assert _basis_before_rotation("Z", "C_ZYX") == "X"
