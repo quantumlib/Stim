c8bf8fbfc5e218701a6ce0d751012a49446dd2dd
Craig Gidney <craiggidney@google.com>
2026-06-26 18:46:19 -0700

    delete constructors

--- src/stim/circuit/circuit.cc
+++ src/stim/circuit/circuit.cc
@@ -12,61 +12,6 @@ using namespace stim;
 Circuit::Circuit() : target_buf(), arg_buf(), tag_buf(), operations(), blocks() {
 }
 
-Circuit::Circuit(const Circuit &circuit)
-    : target_buf(circuit.target_buf.total_allocated()),
-      arg_buf(circuit.arg_buf.total_allocated()),
-      tag_buf(circuit.tag_buf.total_allocated()),
-      operations(circuit.operations),
-      blocks(circuit.blocks) {
-    // Keep local copy of operation data.
-    for (auto &op : operations) {
-        op.targets = target_buf.take_copy(op.targets);
-        op.args = arg_buf.take_copy(op.args);
-        op.tag = tag_buf.take_copy(op.tag);
-    }
-}
-
-Circuit::Circuit(Circuit &&circuit) noexcept
-    : target_buf(std::move(circuit.target_buf)),
-      arg_buf(std::move(circuit.arg_buf)),
-      tag_buf(std::move(circuit.tag_buf)),
-      operations(std::move(circuit.operations)),
-      blocks(std::move(circuit.blocks)) {
-}
-
-Circuit &Circuit::operator=(const Circuit &circuit) {
-    if (&circuit != this) {
-        blocks = circuit.blocks;
-        operations = circuit.operations;
-
-        // Keep local copy of operation data.
-        target_buf = MonotonicBuffer<GateTarget>(circuit.target_buf.total_allocated());
-        arg_buf = MonotonicBuffer<double>(circuit.arg_buf.total_allocated());
-        tag_buf = MonotonicBuffer<char>(circuit.tag_buf.total_allocated());
-        for (auto &op : operations) {
-            op.targets = target_buf.take_copy(op.targets);
-            op.args = arg_buf.take_copy(op.args);
-            op.tag = tag_buf.take_copy(op.tag);
-        }
-    }
-    return *this;
-}
-
-Circuit &Circuit::operator=(Circuit &&circuit) noexcept {
-    if (&circuit != this) {
-        operations = std::move(circuit.operations);
-        blocks = std::move(circuit.blocks);
-        target_buf = std::move(circuit.target_buf);
-        arg_buf = std::move(circuit.arg_buf);
-        tag_buf = std::move(circuit.tag_buf);
-    }
-    return *this;
-}
-
-size_t Circuit::count_qubits() const {
-    return 0;
-}
-
 size_t Circuit::max_lookback() const {
     return 0;
 }
@@ -86,24 +31,8 @@ uint64_t stim::mul_saturate(uint64_t a, uint64_t b) {
     return a * b;
 }
 
-uint64_t Circuit::count_measurements() const {
-    return 0;
-}
-
-uint64_t Circuit::count_detectors() const {
-    return 0;
-}
-
 uint64_t Circuit::count_ticks() const {
     return flat_count_operations([=](const CircuitInstruction &op) -> uint64_t {
         return op.gate_type == GateType::TICK;
     });
-}
-
-uint64_t Circuit::count_observables() const {
-    return 0;
-}
-
-size_t Circuit::count_sweep_bits() const {
-    return 0;
 }
