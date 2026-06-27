7aa1c1e0ebd185c1910dc930e12f8a57020e5936
Craig Gidney <craiggidney@google.com>
2026-06-26 19:42:38 -0700

    restore failure

--- src/stim.pybind.cc
+++ src/stim.pybind.cc
@@ -1,17 +1,22 @@
 #include <pybind11/pybind11.h>
 
 #include <cstdint>
-#include <iostream>
 
 void repro() {
-    int targets[6]{0, 1, 0, 0, 1, 0};
+    uint32_t targets[6]{
+        0,
+        27,
+        0,
+        0,
+        27,
+        0
+    };
     uint64_t n = 6;
-    for (int k = 0; k < 6; k++) {
-        if (targets[k]) {
+    for (size_t k = 0; k < 6; k++) {
+        if (targets[k] == 27) {
             n -= 2;
         }
     }
-    std::cerr << "n=" << n << "\n";
     if (n != 2) {
         throw std::invalid_argument("WRONG COUNT!");
     }
