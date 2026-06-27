e61f848328c2c243c5f1e3ba404f92ca06871fc0
Craig Gidney <craiggidney@google.com>
2026-06-26 18:12:22 -0700

    restore failure

--- src/stim/circuit/circuit_instruction.cc
+++ src/stim/circuit/circuit_instruction.cc
@@ -20,9 +20,33 @@
 
 using namespace stim;
 
+uint64_t CircuitInstruction::repeat_block_rep_count() const {
+    assert(targets.size() == 3);
+    uint64_t low = targets[1].data;
+    uint64_t high = targets[2].data;
+    return low | (high << 32);
+}
+
+Circuit &CircuitInstruction::repeat_block_body(Circuit &host) const {
+    assert(targets.size() == 3);
+    auto b = targets[0].data;
+    assert(b < host.blocks.size());
+    return host.blocks[b];
+}
+
+const Circuit &CircuitInstruction::repeat_block_body(const Circuit &host) const {
+    assert(targets.size() == 3);
+    auto b = targets[0].data;
+    assert(b < host.blocks.size());
+    return host.blocks[b];
+}
+
 CircuitInstruction::CircuitInstruction(
     GateType gate_type, SpanRef<const double> args, SpanRef<const GateTarget> targets, std::string_view tag)
     : gate_type(gate_type), args(args), targets(targets), tag(tag) {
+}
+
+void CircuitInstruction::validate() const {
 }
 
 uint64_t CircuitInstruction::count_measurement_results() const {
@@ -35,4 +59,84 @@ uint64_t CircuitInstruction::count_measurement_results() const {
     }
     std::cerr << "count final " << n << "\n";
     return n;
+}
+
+bool CircuitInstruction::can_fuse(const CircuitInstruction &other) const {
+    auto flags = GATE_DATA[gate_type].flags;
+    return gate_type == other.gate_type && args == other.args && !(flags & GATE_IS_NOT_FUSABLE) && tag == other.tag;
+}
+
+bool CircuitInstruction::operator==(const CircuitInstruction &other) const {
+    return gate_type == other.gate_type && args == other.args && targets == other.targets && tag == other.tag;
+}
+bool CircuitInstruction::approx_equals(const CircuitInstruction &other, double atol) const {
+    if (gate_type != other.gate_type || targets != other.targets || args.size() != other.args.size() ||
+        tag != other.tag) {
+        return false;
+    }
+    for (size_t k = 0; k < args.size(); k++) {
+        if (fabs(args[k] - other.args[k]) > atol) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool CircuitInstruction::operator!=(const CircuitInstruction &other) const {
+    return !(*this == other);
+}
+
+std::ostream &stim::operator<<(std::ostream &out, const CircuitInstruction &instruction) {
+    out << GATE_DATA[instruction.gate_type].name;
+    if (!instruction.tag.empty()) {
+        out << '[';
+        write_tag_escaped_string_to(instruction.tag, out);
+        out << ']';
+    }
+    if (!instruction.args.empty()) {
+        out << '(';
+        bool first = true;
+        for (auto e : instruction.args) {
+            if (first) {
+                first = false;
+            } else {
+                out << ", ";
+            }
+            if (e > (double)INT64_MIN && e < (double)INT64_MAX && (int64_t)e == e) {
+                out << (int64_t)e;
+            } else {
+                out << e;
+            }
+        }
+        out << ')';
+    }
+    write_targets(out, instruction.targets);
+    return out;
+}
+
+void stim::write_tag_escaped_string_to(std::string_view tag, std::ostream &out) {
+    for (char c : tag) {
+        switch (c) {
+            case '\n':
+                out << "\\n";
+                break;
+            case '\r':
+                out << "\\r";
+                break;
+            case '\\':
+                out << "\\B";
+                break;
+            case ']':
+                out << "\\C";
+                break;
+            default:
+                out << c;
+        }
+    }
+}
+
+std::string CircuitInstruction::str() const {
+    std::stringstream s;
+    s << *this;
+    return s.str();
 }
