47f65c09c49ade63b4251f74ffe0fd6798f6174e
Craig Gidney <craiggidney@google.com>
2026-06-26 18:17:26 -0700

    delete for_combined_target_groups

--- src/stim/circuit/circuit_instruction.h
+++ src/stim/circuit/circuit_instruction.h
@@ -26,52 +26,22 @@ namespace stim {
 
 struct Circuit;
 
-/// The data that describes how a gate is being applied to qubits (or other targets).
-///
-/// A gate applied to targets.
-///
-/// This struct is not self-sufficient. It points into data stored elsewhere (e.g. in a Circuit's jagged_data).
 struct CircuitInstruction {
     /// The gate applied by the operation.
     GateType gate_type;
 
-    /// Numeric arguments varying the functionality of the gate.
-    ///
-    /// The meaning of the numbers varies from gate to gate.
-    /// Examples:
-    ///     X_ERROR(p) has a single argument: probability of X.
-    ///     PAULI_CHANNEL_1(px,py,pz) has multiple probability arguments.
-    ///     DETECTOR(c1,c2) has variable arguments: coordinate data.
-    ///     OBSERVABLE_INCLUDE(k) has a single argument: the observable index.
     SpanRef<const double> args;
-
-    /// Encoded data indicating the qubits and other targets acted on by the gate.
     SpanRef<const GateTarget> targets;
-
-    /// Arbitrary string associated with the instruction.
-    /// No effect on simulations or analysis steps within stim, but user code may use it.
     std::string_view tag;
 
     CircuitInstruction() = delete;
     CircuitInstruction(
         GateType gate_type, SpanRef<const double> args, SpanRef<const GateTarget> targets, std::string_view tag);
-
-    /// Determines if two operations can be combined into one operation (with combined targeting data).
-    ///
-    /// For example, `H 1` then `H 2 1` is equivalent to `H 1 2 1` so those instructions are fusable.
     bool can_fuse(const CircuitInstruction &other) const;
-    /// Equality.
     bool operator==(const CircuitInstruction &other) const;
-    /// Inequality.
     bool operator!=(const CircuitInstruction &other) const;
-    /// Approximate equality.
     bool approx_equals(const CircuitInstruction &other, double atol) const;
-    /// Returns a text description of the instruction, as would appear in a STIM circuit file.
-    std::string str() const;
 
-    /// Determines the number of entries added to the measurement record by the operation.
-    ///
-    /// Note: invalid to use this on REPEAT blocks.
     uint64_t count_measurement_results() const;
 
     uint64_t repeat_block_rep_count() const;
@@ -86,45 +56,7 @@ struct CircuitInstruction {
     /// Raises:
     ///     std::invalid_argument: Validation failed.
     void validate() const;
-
-    template <typename CALLBACK>
-    inline void for_combined_target_groups(CALLBACK callback) const {
-        auto flags = GATE_DATA[gate_type].flags;
-        size_t start = 0;
-        while (start < targets.size()) {
-            size_t end;
-            if (flags & stim::GateFlags::GATE_TARGETS_COMBINERS) {
-                end = start + 1;
-                while (end < targets.size() && targets[end].is_combiner()) {
-                    end += 2;
-                }
-            } else if (flags & stim::GateFlags::GATE_IS_SINGLE_QUBIT_GATE) {
-                end = start + 1;
-            } else if (flags & stim::GateFlags::GATE_TARGETS_PAIRS) {
-                end = start + 2;
-            } else if (
-                (flags & stim::GateFlags::GATE_TARGETS_PAULI_STRING) &&
-                !(flags & stim::GateFlags::GATE_TARGETS_COMBINERS)) {
-                // like CORRELATED_ERROR
-                end = targets.size();
-            } else if (flags & stim::GateFlags::GATE_ONLY_TARGETS_MEASUREMENT_RECORD) {
-                // like DETECTOR
-                end = start + 1;
-            } else if (gate_type == GateType::MPAD || gate_type == GateType::QUBIT_COORDS) {
-                end = start + 1;
-            } else {
-                throw std::invalid_argument("Not implemented: splitting " + str());
-            }
-            std::span<const GateTarget> group = targets.sub(start, end);
-            callback(group);
-            start = end;
-        }
-    }
 };
-
-void write_tag_escaped_string_to(std::string_view tag, std::ostream &out);
-
-std::ostream &operator<<(std::ostream &out, const CircuitInstruction &op);
 
 }  // namespace stim
 
