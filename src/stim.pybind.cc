27fcd07965d7dfb8f3c935091de0c8dd3d440089
Craig Gidney <craiggidney@google.com>
2026-06-26 19:27:06 -0700

    delete Circuit

--- src/stim.pybind.cc
+++ src/stim.pybind.cc
@@ -20,23 +20,6 @@ struct CircuitInstruction {
     }
 };
 
-struct Circuit {
-    std::vector<CircuitInstruction> operations;
-
-    template <typename MAP>
-    uint64_t max_operation_property(const MAP &map) const {
-        uint64_t n = 0;
-        for (const auto &op : operations) {
-            if (op.gate_type == 8) {
-                // Handled in block case.
-                continue;
-            }
-            n = std::max(n, (uint64_t)map(op));
-        }
-        return n;
-    }
-};
-
 PYBIND11_MODULE(STIM_PYBIND11_MODULE_NAME, m) {
     m.def(
         "test",
