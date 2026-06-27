2310318532330bca437943b6fc4d13c67ec49a57
Craig Gidney <craiggidney@google.com>
2026-06-26 19:26:31 -0700

    single file

--- src/stim.pybind.cc
+++ src/stim.pybind.cc
@@ -1,9 +1,41 @@
-#include <pybind11/iostream.h>
 #include <pybind11/pybind11.h>
 
-#include "circuit_instruction.h"
+#include <span>
+#include <cstdint>
+#include <iostream>
 
-using namespace stim;
+struct CircuitInstruction {
+    uint32_t gate_type;
+    std::span<const uint32_t> targets;
+    uint64_t count_measurement_results() const {
+        uint64_t n = (uint64_t)targets.size();
+        std::cerr << "counting start ... " << n << "\n";
+        for (auto e : targets) {
+            if (e == 27) {
+                n -= 2;
+            }
+        }
+        std::cerr << "count final " << n << "\n";
+        return n;
+    }
+};
+
+struct Circuit {
+    std::vector<CircuitInstruction> operations;
+
+    template <typename MAP>
+    uint64_t max_operation_property(const MAP &map) const {
+        uint64_t n = 0;
+        for (const auto &op : operations) {
+            if (op.gate_type == 8) {
+                // Handled in block case.
+                continue;
+            }
+            n = std::max(n, (uint64_t)map(op));
+        }
+        return n;
+    }
+};
 
 PYBIND11_MODULE(STIM_PYBIND11_MODULE_NAME, m) {
     m.def(
