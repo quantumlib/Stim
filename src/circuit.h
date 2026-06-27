e81b3092fe6254844232a277ebca7f227106d4f9
Craig Gidney <craiggidney@google.com>
2026-06-26 19:21:12 -0700

    del saturate

--- src/circuit.h
+++ src/circuit.h
@@ -7,21 +7,6 @@
 
 namespace stim {
 
-inline uint64_t add_saturate(uint64_t a, uint64_t b) {
-    uint64_t r = a + b;
-    if (r < a) {
-        return UINT64_MAX;
-    }
-    return r;
-}
-
-inline uint64_t mul_saturate(uint64_t a, uint64_t b) {
-    if (b && a > UINT64_MAX / b) {
-        return UINT64_MAX;
-    }
-    return a * b;
-}
-
 struct Circuit {
     std::vector<CircuitInstruction> operations;
 
