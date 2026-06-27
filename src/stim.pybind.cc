0fd682c24b79d2313910cc9a413f63448d15f4dd
Craig Gidney <craiggidney@google.com>
2026-06-26 19:36:59 -0700

    restore data

--- src/stim.pybind.cc
+++ src/stim.pybind.cc
@@ -1,28 +1,27 @@
 #include <pybind11/pybind11.h>
 
 #include <cstdint>
-#include <iostream>
 
-uint64_t count_measurement_results(const uint32_t *targets, size_t count) {
-    uint64_t n = (uint64_t)count;
-    for (size_t k = 0; k < count; k++) {
+void repro() {
+    uint32_t targets[6]{
+        0,
+        27,
+        0,
+        0,
+        27,
+        0
+    };
+    uint64_t n = 6;
+    for (size_t k = 0; k < 6; k++) {
         if (targets[k] == 27) {
             n -= 2;
         }
     }
-    return n;
-}
-
-void repro() {
-    uint32_t targets[3]{
-        0,
-        0,
-        27,
-    };
-    if (count_measurement_results(targets, 3) != 1) {
+    if (n != 2) {
         throw std::invalid_argument("WRONG COUNT!");
     }
 }
+
 PYBIND11_MODULE(STIM_PYBIND11_MODULE_NAME, m) {
     m.def("test", &repro);
 }
