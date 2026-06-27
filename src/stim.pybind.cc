8fabdda228e8ef48bbd38c8b17cc3d43c4a7c90b
Craig Gidney <craiggidney@google.com>
2026-06-26 19:35:02 -0700

    spanvec less

--- src/stim.pybind.cc
+++ src/stim.pybind.cc
@@ -1,13 +1,12 @@
 #include <pybind11/pybind11.h>
 
-#include <span>
 #include <cstdint>
 #include <iostream>
 
-uint64_t count_measurement_results(std::span<const uint32_t> targets) {
-    uint64_t n = (uint64_t)targets.size();
-    for (uint32_t e : targets) {
-        if (e == 27) {
+uint64_t count_measurement_results(const uint32_t *targets, size_t count) {
+    uint64_t n = (uint64_t)count;
+    for (size_t k = 0; k < count; k++) {
+        if (targets[k] == 27) {
             n -= 2;
         }
     }
@@ -15,12 +14,12 @@ uint64_t count_measurement_results(std::span<const uint32_t> targets) {
 }
 
 void repro() {
-    std::vector<uint32_t> targets{
+    uint32_t targets[3]{
         0,
         0,
         27,
     };
-    if (count_measurement_results(targets) != 1) {
+    if (count_measurement_results(targets, 3) != 1) {
         throw std::invalid_argument("WRONG COUNT!");
     }
 }
