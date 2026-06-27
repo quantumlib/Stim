61bb0721e889222a0d2669af7e7de5145488ab80
Craig Gidney <craiggidney@google.com>
2026-06-26 19:18:53 -0700

    constify

--- src/circuit.cc
+++ src/circuit.cc
@@ -9,6 +9,6 @@ using namespace stim;
 
 uint64_t Circuit::count_observables() const {
     return max_operation_property([=](const CircuitInstruction &op) -> uint64_t {
-        return op.gate_type == 7 ? 2 : 0;
+        return 1;
     });
 }
