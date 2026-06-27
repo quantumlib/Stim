d419976a529f69b0729ea077a1a905657f781498
Craig Gidney <craiggidney@google.com>
2026-06-26 17:26:41 -0700

    inline circuit instruction construction

--- src/stim/py/stim.pybind.cc
+++ src/stim/py/stim.pybind.cc
@@ -12,10 +12,22 @@ PYBIND11_MODULE(STIM_PYBIND11_MODULE_NAME, m) {
     m.def(
         "test",
         []() -> int {
-            Circuit circuit(R"CIRCUIT(
-                MPP X0*X1 Y0*Y1
-            )CIRCUIT");
-            size_t num_measure = circuit.operations[0].count_measurements();
+            std::vector<GateTarget> targets{
+                GateTarget::x(0),
+                GateTarget::combiner(),
+                GateTarget::x(1),
+                GateTarget::y(0),
+                GateTarget::combiner(),
+                GateTarget::y(1),
+            };
+            CircuitInstruction inst{
+                GateType::MPP,
+                {},
+                targets,
+                "",
+            };
+
+            size_t num_measure = inst.count_measurement_results();
             if (num_measure != 2) {
                 throw std::invalid_argument("WRONG COUNT!");
             }
